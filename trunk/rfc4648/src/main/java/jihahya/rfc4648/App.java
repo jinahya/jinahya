920c02dab321742055088a158b71b93bc6cb8c17
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2008-07-18 05:17:32 +0000

    ^^*

---
+++ trunk/rfc4648/src/main/java/jihahya/rfc4648/App.java
@@ -0,0 +1,13 @@
+package jihahya.rfc4648;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
