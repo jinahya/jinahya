03be257a92393a1c8253964c8ec0a4dd6233c64f
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2009-02-11 07:17:24 +0000

    ^^*

--- trunk/jinahya/src/main/java/jinahya/io/BitInput.java
+++ trunk/jinahya/src/main/java/jinahya/io/BitInput.java
@@ -10,21 +10,42 @@ import java.io.IOException;
 public interface BitInput {
 
     /**
-     * @param length
-     * @exception IllegalArgumentException
-     * @exception IOException
+     * Reads a signed integer.
+     *
+     * @param length number of bits
+     * @exception IllegalArgumentException length <= 0 || length > 32
+     * @exception IOException if an I/O error occurs
      */
     public int readInt(int length) throws IOException;
 
+
     /**
+     * Reads an unsigned integer.
+     *
+     * @param length number of bits
+     * @throws IllegalArgumentException length <= 0 || length >= 32
+     * @exception IOException if an I/O error occurs
      */
     public int readUnsignedInt(int length) throws IOException;
 
+
     /**
+     * Reads a signed long.
+     *
+     * @param length number of bits
+     * @exception IllegalArgumentException length <= 0 || length > 64
+     * @exception IOException if an I/O error occurs
      */
     public long readLong(int length) throws IOException;
 
+
     /**
+     * Reads an unsigned long.
+     *
+     * @param length number of bits
+     * @exception IllegalArgumentException length <= 0 || length > 64
+
+     * @exception IOException if an I/O error occurs
      */
     public long readUnsignedLong(int length) throws IOException;
 }
