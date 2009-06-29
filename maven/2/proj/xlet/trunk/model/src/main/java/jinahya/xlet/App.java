bcf5d05950d73b38bb553c4371747ed5db2624db
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2009-06-29 09:02:11 +0000

    ^^*

---
+++ maven/2/proj/xlet/trunk/model/src/main/java/jinahya/xlet/App.java
@@ -0,0 +1,13 @@
+package jinahya.xlet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
