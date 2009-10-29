136ddabf8f60adb4612172faf1db8df66fa735e0
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2009-10-29 03:40:18 +0000

    ^^*

---
+++ maven/2/proj/jinahya.util/state-machine-javatv-xlet-support-example/trunk/src/main/java/xlet/Impl.java
@@ -0,0 +1,39 @@
+/*
+ *  Copyright 2009 onacit.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package xlet;
+
+
+import jinahya.util.javatv.xlet.AbstractXlet;
+
+
+/**
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ */
+public class Impl extends AbstractXlet {
+
+    // "xlet.Impl".hashCode();
+    // -91910485
+
+    public static final int IDENTIFIER = 0xFA858EAB;
+
+
+    public Impl() {
+        super(IDENTIFIER);
+    }
+}
