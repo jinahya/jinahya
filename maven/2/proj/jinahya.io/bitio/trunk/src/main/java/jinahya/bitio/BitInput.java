3f302f36996a987dff6f9c782d6c4d14a190dfae
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2009-11-05 10:03:43 +0000

    ^^*

--- maven/2/proj/jinahya.io/bitio/trunk/src/main/java/jinahya/bitio/BitInput.java
+++ maven/2/proj/jinahya.io/bitio/trunk/src/main/java/jinahya/bitio/BitInput.java
@@ -1,15 +1,62 @@
 package jinahya.bitio;
 
 
+import java.io.EOFException;
 import java.io.IOException;
+import java.io.InputStream;
 
 
 /**
- * An interface for read bits.
+ *
  *
  * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
  */
-public interface BitInput {
+public class BitInput {
+
+
+    /**
+     *
+     * @param in
+     */
+    public BitInput(final InputStream in) {
+        this(new ByteInput() {
+            public int readByte() throws IOException {
+                return in.read();
+            }
+        });
+    }
+
+
+    /**
+     *
+     * @param input
+     */
+    public BitInput(final ByteInput input) {
+        super();
+
+        this.input = input;
+    }
+
+
+    private static final int[] powers  = new int[] {
+        0x01, // 1   0000 0001
+        0x03, // 3   0000 0011
+        0x07, // 7   0000 0111
+        0x0F, // 15  0000 1111
+        0x1F, // 31  0001 1111
+        0x3F, // 63  0011 1111
+        0x7F, // 127 0111 1111
+    };
+
+
+    /**
+     *
+     * @return
+     * @throws IOException
+     */
+    public boolean readBoolean() throws IOException {
+        return readUnsignedByte(1) == 0x01;
+    }
 
 
     /**
@@ -18,7 +65,31 @@ public interface BitInput {
      * @return
      * @throws IOException
      */
-    public int readInt(int length) throws IOException;
+    public int readUnsignedByte(int length) throws IOException {
+
+        if (length < 1 || length >= 8) {
+            throw new IllegalArgumentException("illegal length: " + length);
+        }
+
+        if (avail == 0x00) {
+            if ((octet = input.readByte()) == -1) {
+                throw new EOFException("EOF");
+            }
+            avail = 0x08;
+        }
+
+        if (avail >= length) {
+            avail -= length;
+            count += length;
+            int value = (octet >> length);
+            octet &= powers[avail - 1];
+            return value;
+        } else {
+            int requi = length - avail;
+            return ((readUnsignedByte(avail) << requi) |
+                     readUnsignedByte(requi));
+        }
+    }
 
 
     /**
@@ -27,7 +98,28 @@ public interface BitInput {
      * @return
      * @throws IOException
      */
-    public int readUnsignedInt(int length) throws IOException;
+    public int readUnsignedShort(int length) throws IOException {
+
+        if (length < 1 || length >= 16) {
+            throw new IllegalArgumentException("illegal length: " + length);
+        }
+
+        int value = 0x00;
+
+        int quotient = length / 7;
+        for (int i = 0; i < quotient; i++) {
+            value <<= 7;
+            value |= readUnsignedByte(7);
+        }
+
+        int remainder = length % 7;
+        if (remainder > 0) {
+            value <<= remainder;
+            value |= readUnsignedByte(remainder);
+        }
+
+        return value;
+    }
 
 
     /**
@@ -36,7 +128,28 @@ public interface BitInput {
      * @return
      * @throws IOException
      */
-    public long readLong(int length) throws IOException;
+    public int readUnsignedInt(int length) throws IOException {
+
+        if (length < 1 || length >= 32) {
+            throw new IllegalArgumentException("illegal length: " + length);
+       }
+
+        int value = 0x00;
+
+        int quotient = length / 15;
+        for (int i = 0; i < quotient; i++) {
+            value <<= 15;
+            value |= readUnsignedShort(15);
+        }
+
+        int remainder = length % 15;
+        if (remainder > 0) {
+            value <<= remainder;
+            value |= readUnsignedShort(remainder);
+        }
+
+        return value;
+    }
 
 
     /**
@@ -45,21 +158,90 @@ public interface BitInput {
      * @return
      * @throws IOException
      */
-    public long readUnsignedLong(int length) throws IOException;
+    public int readInt(int length) throws IOException {
+
+        if (length <= 1 || length > 32) {
+            throw new IllegalArgumentException("illegal length: " + length);
+        }
+
+        int value = (readBoolean() ? 0 : -1) << (length - 1);
+
+        return value | readUnsignedInt(length -1);
+    }
 
 
     /**
-     * Returns the number of bits read.
      *
-     * @return number of bits
+     * @param length
+     * @return
+     * @throws IOException
      */
-    public long getCount();
+    public long readUnsignedLong(int length) throws IOException {
+
+        if (length < 1 || length >= 64) {
+            throw new IllegalArgumentException("illegal length: " + length);
+        }
+
+        long value = 0x00L;
+
+        int quotient = length / 31;
+        for (int i = 0; i < quotient; i++) {
+            value <<= 31;
+            value |= readUnsignedInt(31);
+        }
+
+        int remainder = length % 31;
+        if (remainder > 0) {
+            value <<= remainder;
+            value |= readUnsignedInt(remainder);
+        }
+
+        return value;
+    }
 
 
     /**
-     * Sets the number of bits read.
      *
-     * @param count
+     * @param length
+     * @return
+     * @throws IOException
      */
-    public void setCount(long count);
+    public long readLong(int length) throws IOException {
+
+        if (length <= 1 || length > 64) {
+            throw new IllegalArgumentException("illegal length: " + length);
+        }
+
+        long value = (readBoolean() ? -1L : 0L) << (length - 1);
+
+        return value | readUnsignedLong(length -1);
+    }
+
+
+    /**
+     *
+     * @return
+     */
+    public long getCount() {
+        return count;
+    }
+
+
+    /**
+     *
+     * @throws IOException
+     */
+    public void close() throws IOException {
+        if (avail > 0x00) {
+            readUnsignedByte(avail);
+        }
+    }
+
+
+    private ByteInput input;
+
+    private int octet = 0x00;
+    private int avail = 0x00;
+
+    private long count = 0L;
 }
--- maven/2/proj/jinahya.io/bitio/trunk/src/main/java/jinahya/bitio/BitOutput.java
+++ maven/2/proj/jinahya.io/bitio/trunk/src/main/java/jinahya/bitio/BitOutput.java
@@ -2,13 +2,93 @@ package jinahya.bitio;
 
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 
 /**
  *
+ *
  * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
  */
-public interface BitOutput {
+public class BitOutput {
+
+
+    /**
+     *
+     * @param out
+     */
+    public BitOutput(final OutputStream out) {
+        this(new ByteOutput() {
+            public void writeByte(int b) throws IOException {
+                out.write(b);
+            }
+        });
+    }
+
+
+    /**
+     *
+     * @param output
+     */
+    public BitOutput(final ByteOutput output) {
+        super();
+
+        this.output = output;
+    }
+
+
+    private static final int[] powers  = new int[] {
+        0x01, // 1   0000 0001
+        0x03, // 3   0000 0011
+        0x07, // 7   0000 0111
+        0x0F, // 15  0000 1111
+        0x1F, // 31  0001 1111
+        0x3F, // 63  0011 1111
+        0x7F, // 127 0111 1111
+    };
+
+
+    /**
+     *
+     * @param value
+     * @throws IOException
+     */
+    public void writeBoolean(boolean value) throws IOException {
+        writeUnsignedByte(1, value ? 0x01 : 0x00);
+    }
+
+
+    public void writeUnsignedByte(final int length, final int value)
+        throws IOException {
+
+        if (avail >= length) {
+            octet <<= length;
+            octet |= (value & powers[length - 1]);
+            if ((avail -= length) == 0) {
+                output.writeByte(octet);
+                octet = 0x00;
+                avail = 0x08;
+            }
+        } else {
+            int requi = length - avail;
+            writeUnsignedByte(avail, value >> (requi));
+            writeUnsignedByte(requi, value);
+        }
+    }
+
+
+    public void writeUnsignedShort(final int length, final int value)
+        throws IOException {
+
+        int quotient = length / 7;
+        int remainder = length % 7;
+        if (remainder > 0) {
+            writeUnsignedByte(remainder, value >> (quotient * 7));
+        }
+        for (int i = quotient - 1; i >= 0; i--) {
+            writeUnsignedByte(7, value >> (i * 7));
+        }
+    }
 
 
     /**
@@ -17,7 +97,22 @@ public interface BitOutput {
      * @param value
      * @throws IOException
      */
-    public void writeInt(int length, int value) throws IOException;
+    public void writeUnsignedInt(final int length, final int value)
+        throws IOException {
+
+        if (length < 1 || length >= 32) {
+            throw new IllegalArgumentException("illegal length: " + length);
+        }
+
+        int quotient = length / 15;
+        int remainder = length % 15;
+        if (remainder > 0) {
+            writeUnsignedShort(remainder, value >> (quotient * 15));
+        }
+        for (int i = quotient -1; i >= 0; i--) {
+            writeUnsignedShort(15, value >> (i * 15));
+        }
+    }
 
 
     /**
@@ -26,7 +121,15 @@ public interface BitOutput {
      * @param value
      * @throws IOException
      */
-    public void writeUnsignedInt(int length, int value) throws IOException;
+    public void writeInt(final int length, final int value) throws IOException {
+
+        if (length <= 1 || length > 32) {
+            throw new IllegalArgumentException("illegal length: " + length);
+        }
+
+        writeBoolean(value < 0);
+        writeUnsignedInt(length - 1, value);
+    }
 
 
     /**
@@ -35,7 +138,22 @@ public interface BitOutput {
      * @param value
      * @throws IOException
      */
-    public void writeLong(int length, long value) throws IOException;
+    public void writeUnsignedLong(final int length, final long value)
+        throws IOException {
+
+        if (length < 1 || length >= 64) {
+            throw new IllegalArgumentException("illegal length: " + length);
+        }
+
+        int quotient = length / 31;
+        int remainder = length % 31;
+        if (remainder > 0) {
+            writeUnsignedInt(remainder, (int) (value >>> (quotient * 31)));
+        }
+        for (int i = quotient - 1; i >= 0; i--) {
+            writeUnsignedInt(31, (int) ((value >> (i * 31)) & 0x7FFFFFFFL));
+        }
+    }
 
 
     /**
@@ -44,19 +162,41 @@ public interface BitOutput {
      * @param value
      * @throws IOException
      */
-    public void writeUnsignedLong(int length, long value) throws IOException;
+    public void writeLong(int length, long value) throws IOException {
+
+        if (length <= 1 || length > 64) {
+            throw new IllegalArgumentException("illegal length: " + length);
+        }
+
+        writeBoolean(value < 0L);
+        writeUnsignedLong(length - 1, value);
+    }
 
 
     /**
      *
      * @return
      */
-    public long getCount();
+    public long getCount() {
+        return count;
+    }
 
 
     /**
      *
-     * @param count
+     * @throws IOException
      */
-    public void setCount(long count);
+    public void close() throws IOException {
+        if (avail < 0x08) {
+            writeUnsignedByte(avail, 0x00);
+        }
+    }
+
+
+    private ByteOutput output;
+
+    private int octet = 0x00;
+    private int avail = 0x08;
+
+    private long count = 0L;
 }
