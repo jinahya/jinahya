15892f809ea8484312085a2e6bdc0d54e38c8157
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2009-11-27 06:39:52 +0000

    ^^*

---
+++ maven/2/proj/jinahya.fsm/fsm-xlet-example/trunk/src/main/java/xlet/Impl2.java
@@ -0,0 +1,86 @@
+/*
+ *  Copyright 2009 onacit.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package xlet;
+
+
+import javax.tv.xlet.Xlet;
+import javax.tv.xlet.XletContext;
+import javax.tv.xlet.XletStateChangeException;
+
+import jinahya.fsm.StateMachineException;
+
+import jinahya.fsm.xlet.XletSupport;
+
+
+/**
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ */
+public class Impl2 implements Xlet {
+
+
+    //@Override
+    public void initXlet(XletContext ctx) throws XletStateChangeException {
+        try {
+            fsms = XletSupport.createInInitXlet(new TaskFactoryImpl());
+        } catch (StateMachineException fsme) {
+            fsme.printStackTrace();
+        }
+
+        try {
+            fsms.initXletInvoked();
+        } catch (StateMachineException fsme) {
+            fsme.printStackTrace();
+        }
+    }
+
+
+    //@Override
+    public void startXlet() throws XletStateChangeException {
+        try {
+            fsms.startXletInvoked();
+        } catch (StateMachineException fsme) {
+            fsme.printStackTrace();
+        }
+    }
+
+
+    //@Override
+    public void pauseXlet() {
+        try {
+            fsms.pauseXletInvoked();
+        } catch (StateMachineException fsme) {
+            fsme.printStackTrace();
+        }
+    }
+
+
+    //@Override
+    public void destroyXlet(boolean unconditional)
+        throws XletStateChangeException {
+
+        try {
+            fsms.destroyXletInvoked();
+        } catch (StateMachineException fsme) {
+            fsme.printStackTrace();
+        }
+    }
+
+
+    private XletSupport fsms;
+}
