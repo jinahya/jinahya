0405f604ab22997186854da2a2702bb24b863706
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2009-11-27 04:51:35 +0000

    ^^*

---
+++ maven/2/proj/jinahya.fsm/fsm/trunk/src/main/java/jinahya/fsm/Transition.java
@@ -0,0 +1,84 @@
+/*
+ *  Copyright 2009 Jin Kwon.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package jinahya.fsm;
+
+
+/**
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ */
+public class Transition {
+
+
+    /**
+     * Creates an instance.
+     *
+     * @param sourceState source state of this transition
+     * @param targetState target state of this transition
+     */
+    public Transition(final State sourceState, final State targetState) {
+        super();
+
+        if (sourceState == null) {
+            throw new IllegalArgumentException("illegal sourceState: null");
+        }
+
+        if (targetState == null) {
+            throw new IllegalArgumentException("illegal targetState: null");
+        }
+
+        /*
+        if (sourceState.equals(targetState)) {
+            throw new IllegalArgumentException("transition for same states");
+        }
+         */
+
+        this.sourceState = sourceState;
+        this.targetState = targetState;
+    }
+
+
+    /**
+     * Return the source state of this transition.
+     *
+     * @return source state
+     */
+    public State getSourceState() {
+        return sourceState;
+    }
+
+
+    /**
+     * Returns the target state of this transition.
+     *
+     * @return target state
+     */
+    public State getTargetState() {
+        return targetState;
+    }
+
+
+    //@Override
+    public String toString() {
+        return "TRANSITION: " + sourceState + " -> " + targetState;
+    }
+
+
+    private State sourceState;
+    private State targetState;
+}
