7876c34f9843547216015dc90bf84d23af18bf11
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2010-03-09 09:57:14 +0000

    ^^*

--- maven/2/proj/jinahya.util/event-listener-support-jdk15/trunk/build.sh
+++ maven/2/proj/jinahya.util/event-listener-support-jdk15/trunk/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
+mvn clean test
+if [ "$?" -ne "0" ]; then exit "$?"; fi
 mvn -P jdk16 install deploy
-if [ "$?" -ne "0" ]; then exit "$?"; fi
 mvn clean install deploy site site:deploy
-if [ "$?" -ne "0" ]; then exit "$?"; fi
 
