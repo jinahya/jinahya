f85dd2fea2edd34f92f19b5aef04c987dc5c8b0c
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2010-05-15 14:50:15 +0000

    ^^*

---
+++ maven/2/proj/jinahya.rfc/rfc-2616/trunk/src/test/java/jinahya/rfc2616/RequestMessageTest.java
@@ -0,0 +1,43 @@
+/*
+ *  Copyright 2010 Jin Kwon.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package jinahya.rfc2616;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.testng.annotations.Test;
+
+
+/**
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ */
+public class RequestMessageTest {
+
+
+    @Test
+    public void readSample() throws IOException {
+        InputStream stream = getClass().getResourceAsStream("/request.bin");
+        RequestMessage message = new RequestMessage();
+        message.read(stream);
+        System.out.println(message);
+        System.out.println(message.getMethod());
+        System.out.println(message.getRequestURI());
+        System.out.println(message.getHTTPVersion());
+    }
+}
