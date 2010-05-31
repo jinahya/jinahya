59ce458ac2316f0081cb8a13566033b8287b4b37
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2010-05-31 09:22:18 +0000

    ^^*

---
+++ maven/2/proj/jinahya.xml/jaxp-utils/trunk/src/test/java/jinahya/xml/xpath/NodePathFactoryTest.java
@@ -0,0 +1,74 @@
+/*
+ *  Copyright 2010 Jin Kwon.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package jinahya.xml.xpath;
+
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import org.w3c.dom.Document;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+
+/**
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ */
+public class NodePathFactoryTest {
+
+
+    /**
+     *
+     * @param builder
+     * @param xPath
+     * @param xml
+     * @return
+     * @throws ParserConfigurationException
+     * @throws SAXException
+     * @throws IOException
+     */
+    public static NodePath<Document> load(final DocumentBuilder builder,
+                                          final XPath xPath, final String xml)
+        throws ParserConfigurationException, SAXException, IOException {
+
+        final NodePathFactory factory =
+            NodePathFactory.newInstance(builder, xPath);
+
+        return factory.newDocumentPath(new InputSource(new StringReader(xml)));
+    }
+
+
+    @Test
+    public void test() throws ParserConfigurationException, SAXException,
+                              IOException {
+
+        final NodePath<Document> documentPath =
+            load(null, null, NodePathTest.XML);
+
+        Assert.assertNotNull(documentPath);
+    }
+}
