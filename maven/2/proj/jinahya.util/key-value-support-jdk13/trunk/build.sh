aeddcda30a19ad99cd10a1d461ba74f5c447d055
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2010-06-29 02:36:31 +0000

    updated

--- maven/2/proj/jinahya.util/key-value-support-jdk13/trunk/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-mvn -P jdk14 clean install deploy
-if [ "$?" -ne "0" ]; then exit "$?"; fi
-mvn -P jdk13 clean install deploy
-if [ "$?" -ne "0" ]; then exit "$?"; fi
-mvn -P jdk12 clean install deploy
-if [ "$?" -ne "0" ]; then exit "$?"; fi
-mvn clean install deploy site site:deploy
-if [ "$?" -ne "0" ]; then exit "$?"; fi
--- maven/2/proj/jinahya.util/key-value-support-jdk15/trunk/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mvn -P jdk16 clean install deploy
-if [ "$?" -ne "0" ]; then exit "$?"; fi
-mvn clean install deploy site site:deploy
-if [ "$?" -ne "0" ]; then exit "$?"; fi
-
