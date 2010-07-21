ed16db8d1b27ff6883a8e530ec512c171755e9ae
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2010-07-21 09:43:35 +0000

    updated

--- maven/2/proj/jinahya.xml/xml-ext/trunk/src/test/java/jinahya/xml/nodepath/NodePathTest.java
+++ maven/2/proj/jinahya.xml/xml-ext/trunk/src/test/java/jinahya/xml/nodepath/NodePathTest.java
@@ -12,16 +12,20 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- *  under the License.
  */
 
 package jinahya.xml.nodepath;
 
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringReader;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
 import javax.xml.xpath.XPathExpressionException;
 
 import org.testng.annotations.Test;
@@ -31,6 +35,9 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 
 /**
@@ -58,4 +65,40 @@ public class NodePathTest {
         + "  </d>"
         + "</a>";
 
+
+    @Test
+    public void printBooks2DotXml()
+        throws ParserConfigurationException, SAXException, IOException,
+               XPathExpressionException {
+
+        final InputStream books2DotXmlStream =
+            NodePathTest.class.getResourceAsStream("/books2.xml");
+
+        final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        final DocumentBuilder db = dbf.newDocumentBuilder();
+
+        final XPathFactory xpf = XPathFactory.newInstance();
+
+        final Node n = db.parse(books2DotXmlStream);
+        final XPath xp = xpf.newXPath();
+
+        final NodePath np = new NodePath(n, xp);
+
+        final NodeList bookNodeList = np.NODESET("/catalog/book");
+        final int length = bookNodeList.getLength();
+        for (int i = 0; i < length; i++) {
+            final Node bookNode = bookNodeList.item(i);
+            final NodePath bookPath = np.child(bookNode);
+            System.out.println("---------------------------------------------");
+            System.out.println("id: " + bookPath.STRING("@id"));
+            System.out.println("author: " + bookPath.STRING("author/text()"));
+            System.out.println("title: " + bookPath.STRING("title/text()"));
+            System.out.println("genre: " + bookPath.STRING("genre/text()"));
+            System.out.println("price: " + bookPath.FLOAT("price/text()"));
+            System.out.println("publish_date: " + bookPath.STRING("publish_date/text()"));
+            System.out.println("description: " + bookPath.STRING("description/text()"));
+
+
+        }
+    }
 }
