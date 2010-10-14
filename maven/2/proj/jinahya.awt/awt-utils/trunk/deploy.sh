350076edd2898f52f9e7f28a0616a1e2cc64f7b5
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2010-10-14 07:07:16 +0000

    updated

--- maven/2/proj/jinahya.awt/awt-utils/trunk/deploy.sh
+++ maven/2/proj/jinahya.awt/awt-utils/trunk/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-mvn -Pjsr14 clean install deploy;mvn clean install deploy site-deploy
+#mvn -Pjsr14 clean install deploy;mvn clean install deploy site-deploy
+mvn -Pjsr14 clean install deploy;mvn clean install deploy
 
