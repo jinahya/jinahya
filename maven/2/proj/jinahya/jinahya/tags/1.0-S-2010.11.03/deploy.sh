8a3c18f88e01f7467a8fd19c0beeda3348e77fca
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2010-11-03 07:24:24 +0000

    tagged 1.0-S-2010.11.03

---
+++ maven/2/proj/jinahya/jinahya/tags/1.0-S-2010.11.03/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+svn up
+mvn dependency:purge-local-repository clean test
+if [ "$?" == "0" ]; then
+    svn ci -m updated
+else
+    echo "--- NG ---"
+    exit $?
+fi
+mvn -Pjsr14 clean deploy
+mvn clean javadoc:jar source:jar deploy
+exit 0
