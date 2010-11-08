309ae60c06d95f0720dbaafca3e87e8c5d177d3a
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2010-11-08 08:36:56 +0000

    bound-xlet-example added

---
+++ maven/2/proj/com.kt.iptv/bound-xlet-example/trunk/src/main/java/com/kt/iptv/ObfuscateMe.java
@@ -0,0 +1,17 @@
+package com.kt.iptv;
+
+
+/**
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ */
+public class ObfuscateMe {
+
+
+    /**
+     * Do very confidential jobs.
+     */
+    public void secret() {
+        System.out.println("Somethings comin' over me. My baby's got a secret.");
+    }
+}
