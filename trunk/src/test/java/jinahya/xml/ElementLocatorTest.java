e25463ee44adadbca8fc0d046611693fc51a2486
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-03-23 14:42:07 +0000

    updated

---
+++ trunk/src/test/java/jinahya/xml/ElementLocatorTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2011 Jin Kwon.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package jinahya.xml;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+/**
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ */
+public abstract class ElementLocatorTest<T extends ElementLocator> {
+
+
+    protected static final String SIMPLE_XML =
+        "<?xml version=\"1.0\"?>"
+        + "<root a=\"b\" c=\"d\">"
+        + "  <grandfather a=\"b\" c=\"d\">"
+        + "    <father a=\"b\" c=\"d\">"
+        + "      <child a=\"b\" c=\"d\">"
+        + "        <grandchild a=\"b\" c=\"d\">"
+        + "          <text  a=\"b\" c=\"d\">text</text>"
+        + "        </grandchild>"
+        + "      </child>"
+        + "    </father>"
+        + "  </grandfather>"
+        + "</root>";
+
+
+    protected void testForSimpleXML(final T locator) {
+
+        Assert.assertEquals(locator.getAttribute("a"), "b");
+        Assert.assertEquals(locator.getAttribute("c"), "d");
+
+        final int grandfatherCount = locator.getCount("grandfather");
+        Assert.assertEquals(grandfatherCount, 1);
+
+        locator.locateChild("grandfather", 0);
+    }
+}
