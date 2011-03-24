e1bd8eef54ef497674620f3c3ffe91f41d4a4afe
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-03-24 14:25:03 +0000

    updated

--- src/test/java/jinahya/rfc4648/BaseTest.java
+++ src/test/java/jinahya/rfc4648/BaseTest.java
@@ -19,6 +19,7 @@ package jinahya.rfc4648;
 
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Random;
 
 import org.testng.Assert;
@@ -42,6 +43,59 @@ public abstract class BaseTest<T extends Base> {
             RANDOM.nextBytes(bytes);
             return bytes;
         }
+    }
+
+
+    private static final String[] WELCOMES = {
+        "Salaam", "Dobrodošli", "歡迎", "欢迎", "歡迎", "Vítáme tĕ", "Velkommen",
+        "Welkom", "Bienvenue", "Wolkom", "Willkommen", "Καλώς ορίσατε", "Aloha",
+        "Shalom", "Benvenuto", "ようこそ", "환영합니다", "Тавтай морилогтун",
+        "Bem-vindo", "Bem-vinda", "Bienvenido", "Välkommen", "Mabuhay",
+        "Swaagatham", "Suswaagatham", "Merhaba"};
+
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length == 0) {
+            test(WELCOMES);
+        } else {
+            test(args);
+        }
+    }
+
+
+    private static void test(final String[] strings) throws IOException {
+        for (String string : strings) {
+
+            System.out.printf("%1$5s: %2$s\n", "input", string);
+
+            final byte[] original = string.getBytes("UTF-8");
+
+            System.out.printf("%1$5s: ", "UTF-8");
+            for (byte b : original) {
+                System.out.printf("%1$s", Integer.toHexString(b & 0xFF));
+            }
+            System.out.printf("\n");
+
+            test("base64", new Base64(), original);
+            test("base64url", new Base64URL(), original);
+            test("base32", new Base32(), original);
+            test("base32hex", new Base32HEX(), original);
+            test("base16", new Base16(), original);
+        }
+    }
+
+
+    private static void test(final String name, final Base base,
+                             final byte[] original)
+        throws IOException {
+
+        final char[] encoded = base.encode(original);
+
+        System.out.printf("\t%1$10s: %2$s\n", name, new String(encoded));
+
+        final byte[] decoded = base.decode(encoded);
+        assert Arrays.equals(decoded, original) : "fail";
     }
 
 
