3d396e14476d772d567b1cd13dff7353992ea841
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-04-03 11:50:10 +0000

    updated

--- jinahya/twitter-xauth-client/trunk/src/test/java/jinahya/twitter/xauth/client/authenticator/BouncyCastleAuthenticatorTest.java
+++ jinahya/twitter-xauth-client/trunk/src/test/java/jinahya/twitter/xauth/client/authenticator/BouncyCastleAuthenticatorTest.java
@@ -18,27 +18,15 @@
 package jinahya.twitter.xauth.client.authenticator;
 
 
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-
-
 /**
  *
- * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ * @author <a href="mailto:support@minigate.net">Minigate Co., Ltd.</a>
  */
-public class JCAAuthenticator implements Authenticator {
+public class BouncyCastleAuthenticatorTest
+    extends AuthenticatorTest<BouncyCastleAuthenticator> {
 
 
-    protected static final String ALGORITHM = "HmacSHA1";
-
-
-    @Override
-    public byte[] authenticate(final byte[] key, final byte[] input)
-        throws Exception {
-
-        final Mac mac = Mac.getInstance(ALGORITHM);
-        mac.init(new SecretKeySpec(key, ALGORITHM));
-
-        return mac.doFinal(input);
+    public BouncyCastleAuthenticatorTest() {
+        super(new BouncyCastleAuthenticator());
     }
 }
