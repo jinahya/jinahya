5f7b573632ccfd22c81dda3455dfc0953dda1366
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-04-04 01:26:26 +0000

    [maven-release-plugin]  copy for tag twitter-xauth-client-1.0-alpha-10

---
+++ jinahya/twitter-xauth-client/tags/twitter-xauth-client-1.0-alpha-10/src/test/java/jinahya/twitter/xauth/client/requester/SocketRequesterTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2011 Jin Kwon.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package jinahya.twitter.xauth.client.requester;
+
+
+import jinahya.twitter.xauth.client.authenticator.BouncyCastleAuthenticator;
+import jinahya.twitter.xauth.client.authenticator.JCAAuthenticator;
+
+import org.testng.annotations.Test;
+
+
+/**
+ *
+ * @author <a href="mailto:support@minigate.net">Minigate Co., Ltd.</a>
+ */
+public class SocketRequesterTest extends RequesterTest<SocketRequester> {
+
+
+    public SocketRequesterTest() {
+        super(new SocketRequester());
+    }
+
+
+    @Test(enabled = true)
+    public void testStatusesPublicTimeline() throws Exception {
+
+        super.testStatusesPublicTimeline(new BouncyCastleAuthenticator());
+        super.testStatusesPublicTimeline(new JCAAuthenticator());
+    }
+}
