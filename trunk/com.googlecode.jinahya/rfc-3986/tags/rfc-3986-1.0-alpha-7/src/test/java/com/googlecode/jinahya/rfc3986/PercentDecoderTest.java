88531241b3eeac5c203fd26f61c833e8162ef2cd
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-07-26 04:45:35 +0000

    [maven-release-plugin]  copy for tag rfc-3986-1.0-alpha-7

---
+++ trunk/com.googlecode.jinahya/rfc-3986/tags/rfc-3986-1.0-alpha-7/src/test/java/com/googlecode/jinahya/rfc3986/PercentDecoderTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2011 <a href="mailto:jinahya@gmail.com">Jin Kwon</a>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.rfc3986;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Random;
+
+import org.apache.commons.lang.RandomStringUtils;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+/**
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ */
+public class PercentDecoderTest {
+
+
+    private static final Random RANDOM = new Random();
+
+
+    @Test(invocationCount = 128)
+    public void testDecodeComparingTwoMethods() throws IOException {
+
+        final byte[] encoded = PercentEncoder.encode(
+            RandomStringUtils.random(RANDOM.nextInt(10)));
+
+
+        final String decoded1;
+        {
+            final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PercentDecoder.decode(new ByteArrayInputStream(encoded), baos);
+            baos.flush();
+            decoded1 = new String(baos.toByteArray(), "US-ASCII");
+        }
+
+        final String decoded2;
+        {
+            final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            final Reader reader = new InputStreamReader(
+                new ByteArrayInputStream(encoded), "US-ASCII");
+            PercentDecoder.decode(reader, baos);
+            baos.flush();
+            decoded2 = new String(baos.toByteArray(), "US-ASCII");
+        }
+
+        Assert.assertEquals(decoded1, decoded2);
+    }
+}
+
