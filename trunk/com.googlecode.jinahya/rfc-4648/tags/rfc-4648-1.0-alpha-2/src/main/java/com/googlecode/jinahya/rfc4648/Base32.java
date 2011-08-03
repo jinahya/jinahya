9ab6f3d9d6162ed93d07a8ea610f27e7ca19c956
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-08-03 03:11:49 +0000

    [maven-release-plugin]  copy for tag rfc-4648-1.0-alpha-2

---
+++ trunk/com.googlecode.jinahya/rfc-4648/tags/rfc-4648-1.0-alpha-2/src/main/java/com/googlecode/jinahya/rfc4648/Base32.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2011 Jin Kwon.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.rfc4648;
+
+
+/**
+ * Base 32 codec.
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ * @see <a href="http://goo.gl/MkkJZ">Base 32 Encoding</a>
+ */
+public class Base32 extends Base {
+
+
+    /**
+     * base32 alphabet.
+     */
+    protected static final byte[] ALPHABET = new byte[]{
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+        'Y', 'Z', '2', '3', '4', '5', '6', '7'
+    };
+
+
+    /**
+     * Creates a new instance.
+     */
+    public Base32() {
+        super(ALPHABET, true);
+    }
+}
+
