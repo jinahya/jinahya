d9b18366ef439548444f6674124ecb86db0af955
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-08-05 07:08:40 +0000

    [maven-release-plugin]  copy for tag element-locator-1.0-alpha-6

---
+++ com.googlecode.jinahya/element-locator/tags/element-locator-1.0-alpha-6/src/test/java/com/googlecode/jinahya/xml/DOMFamilyTest.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2011 <a href="mailto:jinahya@gmail.com">Jin Kwon</a>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.xml;
+
+
+import java.io.IOException;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.xml.XMLConstants;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.bootstrap.DOMImplementationRegistry;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSInput;
+import org.w3c.dom.ls.LSOutput;
+import org.w3c.dom.ls.LSParser;
+import org.w3c.dom.ls.LSSerializer;
+
+
+/**
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ */
+public class DOMFamilyTest extends FamilyTest<Document> {
+
+
+    private static final DOMImplementationRegistry REGISTRY;
+
+
+    static {
+        try {
+            REGISTRY = DOMImplementationRegistry.newInstance();
+        } catch (ClassNotFoundException cnfe) {
+            throw new InstantiationError(cnfe.getMessage());
+        } catch (InstantiationException ie) {
+            throw new InstantiationError(ie.getMessage());
+        } catch (IllegalAccessException iae) {
+            throw new InstantiationError(iae.getMessage());
+        }
+    }
+
+
+    private static final DOMImplementationLS IMPLEMENTATION =
+        (DOMImplementationLS) REGISTRY.getDOMImplementation("XML 3.0");
+
+
+    @Override
+    protected Document parseDocument(final InputStream source,
+                                     final String charsetName)
+        throws Exception {
+
+        final LSParser parser = IMPLEMENTATION.createLSParser(
+            DOMImplementationLS.MODE_SYNCHRONOUS,
+            XMLConstants.W3C_XML_SCHEMA_NS_URI);
+
+        final LSInput input = IMPLEMENTATION.createLSInput();
+        input.setByteStream(source);
+        input.setEncoding(charsetName);
+
+        return parser.parse(input);
+    }
+
+
+    @Override
+    protected ElementLocator parseLocator(final Document document)
+        throws Exception {
+
+        return DOMElementLocator.parse(document);
+    }
+
+
+    @Override
+    protected void printLocator(final ElementLocator locator,
+                                final Document document)
+        throws Exception {
+
+        DOMElementLocator.print(locator.getRoot(), document);
+    }
+
+
+    @Override
+    protected void printDocument(final Document document,
+                                 final OutputStream target,
+                                 final String charsetName)
+        throws IOException {
+
+        final LSSerializer serializer = IMPLEMENTATION.createLSSerializer();
+
+        final LSOutput output = IMPLEMENTATION.createLSOutput();
+        output.setByteStream(target);
+        output.setEncoding(charsetName);
+
+        serializer.write(document, output);
+    }
+}
+
---
+++ com.googlecode.jinahya/element-locator/tags/element-locator-1.0-alpha-6/src/test/java/com/googlecode/jinahya/xml/ElementLocatorTest.java
@@ -0,0 +1,205 @@
+/*
+ * Copyright 2011 Jin Kwon.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.xml;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.security.MessageDigest;
+
+import org.apache.commons.codec.binary.Hex;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+/**
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ * @param <D> document type parameter
+ */
+public abstract class ElementLocatorTest<D> {
+
+
+    protected static URL getResource() {
+        return ElementLocatorTest.class.getResource("test.xml");
+    }
+
+
+    @Test
+    public void testRead() throws Exception {
+
+        final URL url = ElementLocatorTest.class.getResource("test.xml");
+        Assert.assertNotNull(url);
+
+        final ElementLocator locator = parseLocator(url.openStream());
+        System.out.println("-------------------------------------------- JSON");
+        System.out.println(locator.getClass());
+        System.out.println(locator.getRoot().toJSON());
+        System.out.println("-------------------------------------------- JSON");
+
+        Assert.assertEquals(locator.getCurrent().namespaceURI, "http://a");
+        Assert.assertEquals(locator.getCurrent().localName, "grandparent");
+
+        Assert.assertEquals(locator.getChildCount("parent"), 2);
+
+        for (int i = 0; i < 2; i++) {
+            locator.locateChild("parent", i);
+            Assert.assertEquals(locator.getText(), "parent" + i);
+            locator.locateParent();
+        }
+
+        for (int i = 0; i < 2; i++) {
+            locator.locateChild("parent", i);
+            Assert.assertEquals(locator.getText(), "parent" + i);
+            locator.locateParent();
+        }
+
+        Assert.assertEquals(locator.getChildCount("http://b", "parent"), 1);
+        locator.locateChild("http://b", "parent", 0);
+
+        Assert.assertEquals(locator.getAttribute("http://a", "a"), "a");
+        Assert.assertNull(locator.getAttribute("", "a"));
+
+        Assert.assertEquals(locator.getAttribute("http://b", "b"), "b");
+        Assert.assertNull(locator.getAttribute("", "b"));
+
+        Assert.assertEquals(locator.getAttribute("http://c", "c"), "c");
+        Assert.assertNull(locator.getAttribute("", "c"));
+
+        Assert.assertEquals(locator.getAttribute("", "d"), "d");
+        Assert.assertNull(locator.getAttribute("http://a", "d"));
+        Assert.assertNull(locator.getAttribute("http://b", "d"));
+        Assert.assertNull(locator.getAttribute("http://c", "d"));
+
+        // --------------------------------------- /a:grandparent/b:parent/child
+        Assert.assertEquals(locator.getChildCount("child"), 3);
+        for (int i = 0; i < 3; i++) {
+            locator.locateChild("child", i);
+            Assert.assertEquals(locator.getText(), "child" + i);
+            locator.locateParent();
+        }
+        Assert.assertEquals(locator.getChildCount("child"), 3);
+        for (int i = 0; i < 3; i++) {
+            locator.locateChild("child", i);
+            Assert.assertEquals(locator.getText(), "child" + i);
+            locator.locateParent();
+        }
+
+        // ------------------------------------- /a:grandparent/b:parent/c:child
+        Assert.assertEquals(locator.getChildCount("http://c", "child"), 1);
+        locator.locateChild("http://c", "child", 0);
+
+        // -------------------------- /a:grandparent/b:parent/c:child/grandchild
+        Assert.assertEquals(locator.getChildCount("", "grandchild"), 1);
+        locator.locateChild("", "grandchild", 0);
+        Assert.assertEquals(locator.getText(), "grandchild");
+    }
+
+
+    protected abstract ElementLocator parseLocator(InputStream in)
+        throws Exception;
+
+
+    protected abstract ElementLocator createLocator(String namespaceURI,
+                                                    String localName)
+        throws Exception;
+
+
+    protected abstract D createDocument() throws Exception;
+
+
+    protected abstract void printElement(ELElement root, D document)
+        throws Exception;
+
+
+    protected abstract void printDocument(D document, OutputStream out,
+                                          String charsetName)
+        throws Exception;
+
+
+    @Test
+    public void testWrite() throws Exception {
+
+        final ElementLocator locator =
+            createLocator("http://a", "grandparent");
+
+        locator.addChild("parent").setText("parent0");
+        locator.locateParent();
+
+        locator.addChild("http://b", "parent");
+        locator.setAttribute("http://a", "a", "a");
+        locator.setAttribute("http://b", "b", "b");
+        locator.setAttribute("http://c", "c", "c");
+        locator.setAttribute("", "d", "d");
+        locator.setAttribute("", "e", "e");
+
+        locator.addChild("child").setText("child0").locateParent();
+        locator.addChild("child").setText("child1").locateParent();
+
+        locator.addChild("http://c", "child").
+            addChild("grandchild").setText("grandchild").
+            locateParent().locateParent();
+
+        locator.addChild("child").setText("child2").locateParent();
+
+        locator.locateParent();
+
+        locator.addChild("parent").setText("parent1");
+
+        locator.locateRoot().addChild("normalizable");
+        locator.setAttribute("", "normalizable", "  \t\n\n\tsdajfsdf    \t\n");
+        locator.setText("    \t\t\\n\nlkjsdfljsaflsdfjsd\t\t\\\t\n\\t");
+
+        locator.locateRoot().addChild("\uD55C\uAE00").setText("\uAD8C\uC9C4");
+
+        final MessageDigest md = MessageDigest.getInstance("SHA-1");
+
+
+        System.out.println("-------------------------------------------- JSON");
+        System.out.println(locator.getClass());
+        final String json = locator.getRoot().toJSON();
+        System.out.println(json);
+        System.out.println("JSON.DIGEST: " + Hex.encodeHexString(
+            md.digest(json.getBytes("US-ASCII"))));
+        System.out.println("-------------------------------------------- JSON");
+
+
+        final D document = createDocument();
+
+        printElement(locator.getRoot(), document);
+
+        System.out.println("--------------------------------------------- XML");
+        System.out.println(locator.getClass());
+
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        final String charsetName = "UTF-8";
+
+        printDocument(document, out, charsetName);
+        out.flush();
+
+        final byte[] bytes = out.toByteArray();
+
+        System.out.println(new String(bytes, charsetName));
+        System.out.println("--------------------------------------------- XML");
+    }
+}
+
