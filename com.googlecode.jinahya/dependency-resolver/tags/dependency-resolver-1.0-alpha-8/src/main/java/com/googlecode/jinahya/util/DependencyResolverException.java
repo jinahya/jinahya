351ac10d178d81e67a56f6190589583e9b868dfe
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-08-16 00:39:05 +0000

    [maven-release-plugin]  copy for tag dependency-resolver-1.0-alpha-8

---
+++ com.googlecode.jinahya/dependency-resolver/tags/dependency-resolver-1.0-alpha-8/src/main/java/com/googlecode/jinahya/util/DependencyResolverException.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2011 <a href="mailto:jinahya@gmail.com">Jin Kwon</a>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.util;
+
+
+/**
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ */
+public class DependencyResolverException extends Exception {
+
+
+    /** GENERATED. */
+    private static final long serialVersionUID = -4398231764430765498L;
+
+
+    public DependencyResolverException(final Throwable cause) {
+        super(cause);
+    }
+
+
+    public DependencyResolverException(final String message,
+                                       final Throwable cause) {
+        super(message, cause);
+    }
+
+
+    public DependencyResolverException(final String message) {
+        super(message);
+    }
+}
+
