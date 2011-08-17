f30d41944d1cc33fc22ead525267a7eba9cd8b34
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-08-17 09:48:33 +0000

    updated

---
+++ com.googlecode.jinahya/xlet-state-machine-example/trunk/src/main/java/com/googlecode/jinahya/util/fsm/UpdateTitlesOnce.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2011 <a href="mailto:jinahya@gmail.com">Jin Kwon</a>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.util.fsm;
+
+
+/**
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ */
+public class UpdateTitlesOnce extends XletInitializationTask {
+
+
+    /**
+     * ID.
+     */
+    public static final String ID = UpdateTitlesOnce.class.getName();
+
+
+    /**
+     * Creates a new instance.
+     */
+    public UpdateTitlesOnce() {
+        super(ID);
+    }
+
+
+    @Override
+    public void prepare(final TransitionContext context) throws FSMException {
+        System.out.println("[" + getId() + "] prepare");
+
+        final Transition transition = context.getTransition();
+        System.out.println(transition);
+
+        System.out.println(XletTransitionMatchers.INIT_XLET.matches(transition));
+
+        super.prepare(context);
+    }
+
+
+    @Override
+    public void prepareOn(final TransitionContext context) throws FSMException {
+
+        System.out.println("[" + getId() + "] prepareOn");
+    }
+
+
+    @Override
+    public void performOn(final TransitionContext context) throws FSMException {
+
+        System.out.println("[" + getId() + "] performOn");
+
+        try {
+            MyXletNYTRSSTitles.getInstance().update();
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
+    }
+}
+
