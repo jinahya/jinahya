2a317653c874765e11d96492effce2efb02d3a79
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-08-19 12:22:04 +0000

    updated

---
+++ com.googlecode.jinahya/_clean.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+wd=`pwd`
+for i in `find . -name pom.xml -printf '%h\n'`
+do
+    if [ ! -d $i/target ]
+    then
+        continue
+    fi
+    echo "[[[[[ " $i " ]]]]]"
+    cd -P $i
+    mvn clean
+    cd $wd
+done
+exit 0
---
+++ com.googlecode.jinahya/_mvn.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if [ $# -eq 0 ]
+then
+    echo no golas
+    exit 1
+fi
+wd=`pwd`
+for i in `find . -name pom.xml -printf '%h\n'`
+do
+    echo "[[[[[ " $i " ]]]]]"
+    cd -P $i
+    mvn $*
+    cd $wd
+done
+exit 0
