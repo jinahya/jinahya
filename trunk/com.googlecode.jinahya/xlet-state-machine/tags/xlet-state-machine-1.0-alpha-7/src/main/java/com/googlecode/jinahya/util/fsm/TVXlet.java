83bda80b9a209dab029049bf0d5af20a8f82135e
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-08-23 04:57:06 +0000

    [maven-release-plugin]  copy for tag xlet-state-machine-1.0-alpha-7

---
+++ trunk/com.googlecode.jinahya/xlet-state-machine/tags/xlet-state-machine-1.0-alpha-7/src/main/java/com/googlecode/jinahya/util/fsm/TVXlet.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2011 <a href="mailto:jinahya@gmail.com">Jin Kwon</a>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.util.fsm;
+
+
+import javax.tv.xlet.Xlet;
+import javax.tv.xlet.XletContext;
+import javax.tv.xlet.XletStateChangeException;
+
+
+/**
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ */
+public abstract class TVXlet implements Xlet {
+
+
+    public TVXlet(final XletMachine machine) {
+        super();
+
+        if (machine == null) {
+            throw new NullPointerException("null machine");
+        }
+
+        this.machine = machine;
+    }
+
+
+    @Override
+    public void initXlet(final XletContext ctx)
+        throws XletStateChangeException {
+
+        try {
+            machine.setState(XletState.PAUSED);
+        } catch (FSMException fsme) {
+            throw new XletStateChangeException(fsme.getMessage());
+        }
+    }
+
+
+    @Override
+    public void startXlet() throws XletStateChangeException {
+
+        try {
+            machine.setState(XletState.ACTIVE);
+        } catch (FSMException fsme) {
+            throw new XletStateChangeException(fsme.getMessage());
+        }
+    }
+
+
+    @Override
+    public void pauseXlet() {
+
+        try {
+            machine.setState(XletState.PAUSED);
+        } catch (FSMException fsme) {
+            fsme.printStackTrace(System.err);
+        }
+    }
+
+
+    @Override
+    public void destroyXlet(final boolean unconditional)
+        throws XletStateChangeException {
+
+        try {
+            machine.setState(XletState.DESTROYED);
+        } catch (FSMException fsme) {
+            fsme.printStackTrace(System.err);
+        }
+    }
+
+
+    /** machine. */
+    private final Machine machine;
+}
+
