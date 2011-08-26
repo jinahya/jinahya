465385635260d44a60916d233843446ad4330053
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-08-26 02:45:54 +0000

    [maven-release-plugin]  copy for tag dependency-resolver-1.0-alpha-10

---
+++ com.googlecode.jinahya/dependency-resolver/tags/dependency-resolver-1.0-alpha-10/src/test/java/com/googlecode/jinahya/util/DependencyResolverTest.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2011 <a href="mailto:jinahya@gmail.com">Jin Kwon</a>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.util;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+/**
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ */
+public class DependencyResolverTest {
+
+
+    @Test
+    public void testAddDependency() throws DependencyResolverException {
+
+        final DependencyResolver<String> resolver =
+            new DependencyResolver<String>();
+
+        try {
+            resolver.addDependency(null, "null");
+            Assert.fail("passed: null source");
+        } catch (NullPointerException npe) {
+            // expected
+        }
+
+        resolver.addDependency("source", null);
+
+        resolver.addDependency("source", "target");
+    }
+
+
+    @Test(expectedExceptions = {IllegalArgumentException.class})
+    public void testAddDependencyForSelfDependency()
+        throws DependencyResolverException {
+
+        final DependencyResolver<String> resolver =
+            new DependencyResolver<String>();
+
+        resolver.addDependency("A", "A");
+    }
+
+
+    @Test(expectedExceptions = {CyclicDependencyResolverException.class})
+    public void testAddDependencyForCyclicDependency()
+        throws DependencyResolverException {
+
+        final DependencyResolver<String> resolver =
+            new DependencyResolver<String>();
+
+        resolver.addDependency("A", "B");
+        resolver.addDependency("B", "C");
+
+        resolver.addDependency("C", "A");
+    }
+
+
+    @Test
+    public void testAddDependencies() throws DependencyResolverException {
+
+        final DependencyResolver<String> resolver =
+            new DependencyResolver<String>();
+
+        try {
+            resolver.addDependencies(null, "null");
+            Assert.fail("passed: null source");
+        } catch (NullPointerException npe) {
+            // expected
+        }
+
+        try {
+            resolver.addDependencies("source", (String[]) null);
+            Assert.fail("passed: null targets");
+        } catch (NullPointerException npe) {
+            // expected
+        }
+
+        resolver.addDependencies("source", "target1", "target2", null);
+    }
+
+
+    @Test
+    public void testHasDependency() throws DependencyResolverException {
+
+        final DependencyResolver<String> resolver =
+            new DependencyResolver<String>();
+
+        Assert.assertFalse(resolver.hasDependency("A", "B"));
+
+        resolver.addDependency("A", "B");
+
+        Assert.assertTrue(resolver.hasDependency("A", "B"));
+
+        resolver.removeDependency("A", "B");
+
+        Assert.assertFalse(resolver.hasDependency("A", "B"));
+    }
+
+
+    @Test
+    public void testRemoveDependency() throws DependencyResolverException {
+
+        final DependencyResolver<String> resolver =
+            new DependencyResolver<String>();
+
+        resolver.removeDependency("A", "B");
+
+        resolver.addDependency("A", "B");
+
+        resolver.removeDependency("A", "B");
+    }
+
+
+    @Test
+    public void testRemoveDependencies() {
+
+        final DependencyResolver<String> resolver =
+            new DependencyResolver<String>();
+
+        resolver.removeDependencies("A", "B", "C", "D", null);
+    }
+}
+
