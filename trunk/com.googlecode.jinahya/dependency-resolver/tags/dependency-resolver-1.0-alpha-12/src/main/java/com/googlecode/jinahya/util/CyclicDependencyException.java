a35e4917e39ff07cff73409a70f15bcc662a1d35
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-09-10 02:18:20 +0000

    [maven-release-plugin]  copy for tag dependency-resolver-1.0-alpha-12

---
+++ trunk/com.googlecode.jinahya/dependency-resolver/tags/dependency-resolver-1.0-alpha-12/src/main/java/com/googlecode/jinahya/util/CyclicDependencyException.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2011 <a href="mailto:jinahya@gmail.com">Jin Kwon</a>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.util;
+
+
+/**
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ */
+public class CyclicDependencyException extends DependencyResolverException {
+
+
+    /** GENERATED. */
+    private static final long serialVersionUID = 8162863238681407043L;
+
+
+    /**
+     * Creates a new dependency for cyclic dependency between
+     * <code>source</code> and <code>target</code>.
+     *
+     * @param source source
+     * @param target target
+     */
+    public CyclicDependencyException(final Object source, final Object target) {
+
+        super("cyclic dependency: " + source + " -> " + target);
+    }
+}
