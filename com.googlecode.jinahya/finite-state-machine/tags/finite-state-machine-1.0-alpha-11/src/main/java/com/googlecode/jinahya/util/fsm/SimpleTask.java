b7b7c1a0c44aa0ada4d2c4a5862fbd09fd66f299
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-09-10 09:02:45 +0000

    [maven-release-plugin]  copy for tag finite-state-machine-1.0-alpha-11

---
+++ com.googlecode.jinahya/finite-state-machine/tags/finite-state-machine-1.0-alpha-11/src/main/java/com/googlecode/jinahya/util/fsm/SimpleTask.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2011 <a href="mailto:jinahya@gmail.com">Jin Kwon</a>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.util.fsm;
+
+
+/**
+ * Task for simple conditions.
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ */
+public abstract class SimpleTask extends SwitchTask {
+
+
+    /** A TransitionMatcher doesn't match at all. */
+    private static final TransitionMatcher NOT_MATCHES_AT_ALL =
+        new TransitionMatcher() {
+
+
+            @Override
+            public boolean matches(final Transition transition) {
+                return false;
+            }
+        };
+
+
+    /**
+     * Creates a new instance.
+     *
+     * @param id task id
+     * @param matchers matchers; not null; not empty
+     */
+    public SimpleTask(final String id, final TransitionMatcher[] matchers) {
+
+        super(id, matchers, new TransitionMatcher[]{NOT_MATCHES_AT_ALL});
+    }
+
+
+    @Override
+    protected void prepareOff(final TransitionContext context)
+        throws FSMException {
+
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+
+    @Override
+    protected void performOff(final TransitionContext context)
+        throws FSMException {
+
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
+
