b985909b7a93fa2c6e2f4a994e6fbe205be6f134
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-09-16 08:01:54 +0000

    updated

---
+++ com.googlecode.jinahya/xlet-state-machine-example/trunk/src/main/java/myxlet/task/initialization/switch_/PrintStateChange.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2011 onacit.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package myxlet.task.initialization.switch_;
+
+
+import com.googlecode.jinahya.util.fsm.FSMException;
+import com.googlecode.jinahya.util.fsm.TransitionContext;
+
+import com.googlecode.jinahya.util.fsm.XletInitializationSwitchTask;
+
+
+/**
+ *
+ * @author onacit
+ */
+public class PrintStateChange extends XletInitializationSwitchTask {
+
+
+    public static final String ID = PrintStateChange.class.getName();
+
+
+    public PrintStateChange() {
+        super(ID);
+    }
+
+
+    @Override
+    public void prepareOn(final TransitionContext context) throws FSMException {
+        System.out.println(getId() + " prepareOn(" + context + ")");
+    }
+
+
+    @Override
+    public void prepareOff(final TransitionContext context)
+        throws FSMException {
+
+        System.out.println(getId() + " prepareOff(" + context + ")");
+    }
+
+
+    @Override
+    public void performOn(final TransitionContext context) throws FSMException {
+        System.out.println(getId() + " performOn(" + context + ")");
+    }
+
+
+    @Override
+    public void performOff(final TransitionContext context)
+        throws FSMException {
+
+        System.out.println(getId() + " performOff(" + context + ")");
+    }
+}
