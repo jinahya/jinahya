198b6b7a307305bc0d430e0ca7943b717c651a1b
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-09-17 10:55:25 +0000

    updated

--- com.googlecode.jinahya/activity-state-machine/trunk/src/main/java/com/googlecode/jinahya/util/fsm/ActivityBackgroundTask.java
+++ com.googlecode.jinahya/activity-state-machine/trunk/src/main/java/com/googlecode/jinahya/util/fsm/ActivityBackgroundTask.java
@@ -23,7 +23,7 @@ package com.googlecode.jinahya.util.fsm;
  *
  * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
  */
-public abstract class ActivityBackgroundTask extends Task {
+public abstract class ActivityBackgroundTask extends AbstractTask {
 
 
     /**
@@ -32,6 +32,6 @@ public abstract class ActivityBackgroundTask extends Task {
      * @param id task id
      */
     public ActivityBackgroundTask(final String id) {
-        super(id, ActivityTransitionMatcher.ON_RESUME);
+        super(id, ActivityTransitionMatcher.ON_START);
     }
 }
