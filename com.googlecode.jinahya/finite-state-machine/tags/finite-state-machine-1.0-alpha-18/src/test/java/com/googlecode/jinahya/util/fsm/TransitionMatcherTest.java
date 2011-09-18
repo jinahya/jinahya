a634d956ad0d981e2a8793bc9e1dc49bf7f54499
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-09-18 15:58:07 +0000

    [maven-release-plugin]  copy for tag finite-state-machine-1.0-alpha-18

---
+++ com.googlecode.jinahya/finite-state-machine/tags/finite-state-machine-1.0-alpha-18/src/test/java/com/googlecode/jinahya/util/fsm/TransitionMatcherTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2011 onacit.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.util.fsm;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+/**
+ *
+ * @author onacit
+ */
+public class TransitionMatcherTest {
+
+
+    @Test
+    public void testYES() throws FSMException {
+
+        try {
+            TransitionMatcher.YES.matches(null);
+            Assert.fail("passed: matches(null)");
+        } catch (NullPointerException npe) {
+            // expected
+        }
+
+        final Machine machine = new MachineImpl();
+
+        final Transition transition = new Transition(
+            machine, machine.getState(), State.INVALID);
+
+        Assert.assertTrue(TransitionMatcher.YES.matches(transition));
+    }
+
+
+    @Test
+    public void testNO() throws FSMException {
+
+        try {
+            TransitionMatcher.NO.matches(null);
+            Assert.fail("passed: matches(null)");
+        } catch (NullPointerException npe) {
+            // expected
+        }
+
+        final Machine machine = new MachineImpl();
+
+        final Transition transition = new Transition(
+            machine, machine.getState(), State.INVALID);
+
+        Assert.assertFalse(TransitionMatcher.NO.matches(transition));
+    }
+}
