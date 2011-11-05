9d01d86b561239cb384c682bcac81820a0989fe5
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-11-05 10:38:25 +0000

    [maven-release-plugin]  copy for tag jdbc-helper-1.0-alpha-4

---
+++ tags/com.googlecode.jinahya/jdbc-helper-1.0-alpha-4/src/main/java/com/googlecode/jinahya/sql/CollectableHelper.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2011 Jin Kwon <jinahya at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.sql;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+/**
+ * A helper for DBCollection.
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+public final class CollectableHelper {
+
+
+    /**
+     * Select a new instance.
+     *
+     * @param <C> collection type parameter
+     * @param <A> accessible type parameter;
+     * @param connection connection
+     * @param tableName element table name
+     * @param idColumnName element id column name
+     * @param collectableType collectable type
+     * @return a new collection instance
+     * @throws SQLException if an SQL error occurs
+     */
+    public static <C extends Collectable<A>, A extends Accessible> C select(
+        final Connection connection, final String tableName,
+        final String idColumnName, final Class<C> collectableType)
+        throws SQLException {
+
+        try {
+            final C collection = collectableType.newInstance();
+            select(connection, tableName, idColumnName, collection);
+            return collection;
+        } catch (IllegalAccessException iae) {
+            throw new SQLException(
+                "failed to create instance of " + collectableType, iae);
+        } catch (InstantiationException ie) {
+            throw new SQLException(
+                "failed to create instance of " + collectableType, ie);
+        }
+    }
+
+
+    /**
+     * Selects given <code>collectio</code>.
+     *
+     * @param <C> collection type parameter
+     * @param <A> accessible type parameter
+     * @param connection connection
+     * @param tableName element table name
+     * @param idColumnName element id column name
+     * @param collectable collectable
+     * @throws SQLException if an SQL error occurs
+     */
+    public static <C extends Collectable<A>, A extends Accessible> void select(
+        final Connection connection, final String tableName,
+        final String idColumnName, final C collectable)
+        throws SQLException {
+
+        if (connection == null) {
+            throw new NullPointerException("null connection");
+        }
+
+        if (tableName == null) {
+            throw new NullPointerException("null elementTableName");
+        }
+
+        if (idColumnName == null) {
+            throw new NullPointerException("null elementIdColumnName");
+        }
+
+        if (collectable == null) {
+            throw new NullPointerException("null element");
+        }
+
+        final PreparedStatement preparedStatement = connection.prepareStatement(
+            "SELECT * FROM " + tableName
+            + " ORDER BY " + idColumnName + " ASC");
+        try {
+            final ResultSet resultSet = preparedStatement.executeQuery();
+            try {
+                final Class<A> accessibleType = collectable.getAccessibleType();
+                while (resultSet.next()) {
+                    try {
+                        final A accessible = accessibleType.newInstance();
+                        accessible.read(resultSet, "");
+                        collectable.getAccessibles().add(accessible);
+                    } catch (IllegalAccessException iae) {
+                        throw new SQLException(
+                            "failed to create instance of " + accessibleType,
+                            iae);
+                    } catch (InstantiationException ie) {
+                        throw new SQLException(
+                            "failed to create instance of " + accessibleType,
+                            ie);
+                    }
+                }
+            } finally {
+                resultSet.close();
+            }
+        } finally {
+            preparedStatement.close();
+        }
+    }
+
+
+    /**
+     * Creates a new instance.
+     */
+    private CollectableHelper() {
+        super();
+    }
+
+
+}
+
