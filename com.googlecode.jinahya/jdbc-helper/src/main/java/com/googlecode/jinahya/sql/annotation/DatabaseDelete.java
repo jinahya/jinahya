4719144f9da2ebf5a4b9d25c7e770406913741a4
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-11-06 08:39:23 +0000

    updated

--- com.googlecode.jinahya/jdbc-helper/src/main/java/com/googlecode/jinahya/sql/annotation/DatabaseDelete.java
+++ com.googlecode.jinahya/jdbc-helper/src/main/java/com/googlecode/jinahya/sql/annotation/DatabaseDelete.java
@@ -32,6 +32,6 @@ import java.lang.annotation.Target;
 @Documented
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface DatabseDelete {
+public @interface DatabaseDelete {
 }
 
