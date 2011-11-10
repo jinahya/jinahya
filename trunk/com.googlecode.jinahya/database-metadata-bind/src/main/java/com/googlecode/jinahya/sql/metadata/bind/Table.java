b3cf35adc2ecf2c11693b6a3608ea0c93746051d
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-11-10 01:57:33 +0000

    updated

--- trunk/com.googlecode.jinahya/database-metadata-bind/src/main/java/com/googlecode/jinahya/sql/metadata/bind/Table.java
+++ trunk/com.googlecode.jinahya/database-metadata-bind/src/main/java/com/googlecode/jinahya/sql/metadata/bind/Table.java
@@ -18,11 +18,8 @@
 package com.googlecode.jinahya.sql.metadata.bind;
 
 
-import java.util.Map;
-
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 
 /**
