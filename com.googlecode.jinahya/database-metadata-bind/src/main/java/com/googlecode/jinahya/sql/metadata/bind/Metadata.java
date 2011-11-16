d62757691dfec3c903618ec3e8f3558e5a959d70
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-11-16 15:05:07 +0000

    Metadata added

---
+++ com.googlecode.jinahya/database-metadata-bind/src/main/java/com/googlecode/jinahya/sql/metadata/bind/Metadata.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2011 Jin Kwon <jinahya at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.sql.metadata.bind;
+
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * Root class of database metadata binding.
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+@XmlRootElement
+@XmlType(propOrder = {"properties", "catalogs"})
+public class Metadata {
+
+
+    /**
+     * Creates a new instance.
+     *
+     * @param connection database connection
+     * @return a new instance of Metadata
+     * @throws SQLException if an SQL error occurs.
+     */
+    public static Metadata newInstance(final Connection connection)
+        throws SQLException {
+
+        return newInstance(connection.getMetaData());
+    }
+
+
+    /**
+     * Creates a new instance.
+     *
+     * @param databaseMetaData database metadata
+     * @return a new instance of Metadata
+     * @throws SQLException if an SQL error occurs.
+     */
+    public static Metadata newInstance(final DatabaseMetaData databaseMetaData)
+        throws SQLException {
+
+        final Metadata instance = new Metadata();
+
+        instance.properties = Properties.newInstance(databaseMetaData);
+
+        instance.catalogs = Catalogs.newInstance(databaseMetaData);
+
+        return instance;
+    }
+
+
+    public Catalogs getCatalogs() {
+        return catalogs;
+    }
+
+
+    public void setCatalogs(final Catalogs catalogs) {
+        this.catalogs = catalogs;
+    }
+
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+
+    public void setProperties(final Properties properties) {
+        this.properties = properties;
+    }
+
+
+    @XmlElement(required = true, nillable = true)
+    private Properties properties;
+
+
+    @XmlElement(required = true, nillable = true)
+    private Catalogs catalogs;
+
+
+}
+
