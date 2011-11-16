c4ae35c0316a4ae109b2a43470f26c62d4c0ddba
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-11-16 15:14:45 +0000

    udpated

--- trunk/com.googlecode.jinahya/database-metadata-bind/src/main/java/com/googlecode/jinahya/sql/metadata/bind/Property.java
+++ trunk/com.googlecode.jinahya/database-metadata-bind/src/main/java/com/googlecode/jinahya/sql/metadata/bind/Property.java
@@ -22,6 +22,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 
 /**
+ * Binding of client property.
  *
  * @author Jin Kwon <jinahya at gmail.com>
  */
@@ -29,41 +30,81 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class Property extends EntrySet {
 
 
+    /**
+     * Returns the value of 'NAME' entry.
+     *
+     * @return 'NAME' entry value
+     */
     public String getNAME() {
         return getValue("NAME");
     }
 
 
+    /**
+     * Sets the value of 'NAME' entry.
+     *
+     * @param NAME 'NAME' entry value
+     */
     public void setNAME(final String NAME) {
         setValue("NAME", NAME);
     }
 
 
+    /**
+     * Returns the value of 'MAX_LEN' entry.
+     *
+     * @return 'MAX_LEN' entry value.
+     */
     public String getMAX_LEN() {
         return getValue("MAX_LEN");
     }
 
 
+    /**
+     * Sets the value of 'MAX_LEN' entry.
+     *
+     * @param MAX_LEN 'MAX_LEN' entry value
+     */
     public void setMAX_LEN(final String MAX_LEN) {
         setValue("MAX_LEN", MAX_LEN);
     }
 
 
+    /**
+     * Returns the value of 'DEFAULT_VALUE' entry.
+     *
+     * @return 'DEFAULT_VALUE' entry value.
+     */
     public String getDEFAULT_VALUE() {
         return getValue("DEFAULT_VALUE");
     }
 
 
+    /**
+     * Sets the value of 'DEFAULT_VALUE' entry.
+     *
+     * @param DEFAULT_VALUE 'DEFAULT_VALUE' entry value.
+     */
     public void setDEFAULT_VALUE(final String DEFAULT_VALUE) {
         setValue("DEFAULT_VALUE", DEFAULT_VALUE);
     }
 
 
+    /**
+     * Returns the value of 'DESCRIPTION' entry.
+     *
+     * @return 'DESCRIPTION' entry value.
+     */
     public String getDESCRIPTION() {
         return getValue("DESCRIPTION");
     }
 
 
+    /**
+     * Sets the value of 'DESCRIPTION' entry.
+     *
+     * @param DESCRIPTION 'DESCRIPTION' entry value
+     */
     public void setDESCRIPTION(final String DESCRIPTION) {
         setValue("DESCRIPTION", DESCRIPTION);
     }
