79bef9c76deb7be37b6e7469a7aff9417f43b4d1
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-11-16 09:59:20 +0000

    updated

---
+++ com.googlecode.jinahya/database-metadata-bind/src/main/java/com/googlecode/jinahya/sql/metadata/bind/MetadataPrinter.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright 2011 Jin Kwon <jinahya at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.sql.metadata.bind;
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+
+import org.kohsuke.args4j.Option;
+
+
+/**
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+public class MetadataPrinter {
+
+
+    public static class ConnectionOption {
+
+
+        public String getDriver() {
+            return driver;
+        }
+
+
+        public void setDriver(final String driver) {
+            this.driver = driver;
+        }
+
+
+        public String getPassword() {
+            return password;
+        }
+
+
+        public void setPassword(final String password) {
+            this.password = password;
+        }
+
+
+        public String getUrl() {
+            return url;
+        }
+
+
+        public void setUrl(final String url) {
+            this.url = url;
+        }
+
+
+        public String getUser() {
+            return user;
+        }
+
+
+        public void setUser(final String user) {
+            this.user = user;
+        }
+
+
+        @Option(name = "-driver", usage = "database driver name")
+        private String driver;
+
+
+        @Option(name = "-url", required = true,
+                usage = "a database url of the form jdbc:subprotocol:subname")
+        private String url;
+
+
+        @Option(name = "-user",
+                usage = "the database user on whose behalf the connection is"
+                        + " being made")
+        private String user;
+
+
+        @Option(name = "-password", usage = "the user's password")
+        private String password;
+
+
+    }
+
+
+    protected static void printMetadata(final ConnectionOption option)
+        throws ClassNotFoundException, SQLException, JAXBException {
+
+        printMetadata(option.driver, option.url, option.user, option.password);
+    }
+
+
+    /**
+     * Prints metadata.
+     *
+     * @param driver driver class name
+     * @param url database connection url
+     * @param user database user
+     * @param password database password
+     * @throws ClassNotFoundException if driver class not found
+     * @throws SQLException if an SQL error occurs.
+     * @throws JAXBException if a JAXB error occurs
+     *
+     * @see Class#forName(String)
+     * @see DriverManager#getConnection(String)
+     * @see DriverManager#getConnection(String, String, String)
+     */
+    protected static void printMetadata(
+        final String driver, final String url, final String user,
+        final String password)
+        throws ClassNotFoundException, SQLException, JAXBException {
+
+        Class.forName(driver);
+
+        final Connection connection;
+        if (user != null && password != null) {
+            connection = DriverManager.getConnection(url, user, password);
+        } else {
+            connection = DriverManager.getConnection(url);
+        }
+        try {
+            final Catalogs catalogs =
+                Catalogs.newInstance(connection.getMetaData());
+            final Marshaller marshaller =
+                DatabaseMetadataBindConstants.JAXB_CONTEXT.createMarshaller();
+            marshaller.setProperty(
+                Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            marshaller.marshal(catalogs, System.out);
+            System.out.flush();
+        } finally {
+            connection.close();
+        }
+    }
+
+
+}
+
