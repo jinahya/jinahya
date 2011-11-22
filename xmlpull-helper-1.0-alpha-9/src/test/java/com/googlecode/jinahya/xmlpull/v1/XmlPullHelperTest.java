cac573bc0d61dfc2e6ad71658027d520de10a8bc
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-11-22 07:43:57 +0000

    [maven-release-plugin]  copy for tag xmlpull-helper-1.0-alpha-9

---
+++ xmlpull-helper-1.0-alpha-9/src/test/java/com/googlecode/jinahya/xmlpull/v1/XmlPullHelperTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2011 Jin Kwon <jinahya at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.xmlpull.v1;
+
+
+import java.io.InputStream;
+
+import java.net.URL;
+
+import java.util.Random;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+import org.xmlpull.v1.XmlSerializer;
+
+
+/**
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+public abstract class XmlPullHelperTest {
+
+
+    protected static final String ENCODING = "UTF-8";
+
+
+    protected static final Random RANDOM = new Random();
+
+
+    protected static final XmlPullParserFactory XML_PULL_PARSER_FACTORY;
+
+
+    static {
+        try {
+            XML_PULL_PARSER_FACTORY = XmlPullParserFactory.newInstance();
+            XML_PULL_PARSER_FACTORY.setNamespaceAware(true);
+        } catch (XmlPullParserException xppe) {
+            throw new InstantiationError(xppe.getLocalizedMessage());
+        }
+    }
+
+
+    protected static XmlPullParser newPullParser()
+        throws XmlPullParserException {
+
+        return XML_PULL_PARSER_FACTORY.newPullParser();
+    }
+
+
+    protected static XmlSerializer newSerializer()
+        throws XmlPullParserException {
+
+        final XmlSerializer serializer = XML_PULL_PARSER_FACTORY.newSerializer();
+
+        assert serializer != null;
+
+        return serializer;
+    }
+
+
+    protected static InputStream getResourceAsStream(final String name) {
+        return XmlPullHelperTest.class.getResourceAsStream(name);
+    }
+
+
+    protected static URL getResource(final String name) {
+        return XmlPullHelperTest.class.getResource(name);
+    }
+
+
+}
+
