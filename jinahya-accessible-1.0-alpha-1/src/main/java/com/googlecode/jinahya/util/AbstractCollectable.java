a84b769b6dec125bb6d53e823d5cd350a910c434
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-11-22 08:42:51 +0000

    [maven-release-plugin]  copy for tag jinahya-accessible-1.0-alpha-1

---
+++ jinahya-accessible-1.0-alpha-1/src/main/java/com/googlecode/jinahya/util/AbstractCollectable.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2011 Jin Kwon <jinahya at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.util;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.xml.bind.annotation.XmlTransient;
+
+
+/**
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ * @param <A> accessible type parameter
+ */
+@XmlTransient
+public class AbstractCollectable<A extends Accessible>
+    implements Collectable<A> {
+
+
+    /**
+     * Creates a new instance.
+     *
+     * @param accessibleType accessibleType
+     */
+    public AbstractCollectable(final Class<A> accessibleType) {
+        super();
+
+        if (accessibleType == null) {
+            throw new NullPointerException("null accessibleType");
+        }
+
+        if (!Accessible.class.isAssignableFrom(accessibleType)) {
+            throw new IllegalArgumentException(
+                accessibleType + " is not assignable to " + Accessible.class);
+        }
+
+        this.accessibleType = accessibleType;
+    }
+
+
+    @Override
+    public final Class<A> getAccessibleType() {
+        return accessibleType;
+    }
+
+
+    @Override
+    public final Collection<A> getAccessibles() {
+
+        if (accessibles == null) {
+            accessibles = new ArrayList<A>();
+        }
+
+        return accessibles;
+    }
+
+
+    /**
+     * accessibleType.
+     */
+    protected final Class<A> accessibleType;
+
+
+    /**
+     * accessibles.
+     */
+    private Collection<A> accessibles;
+
+
+}
+
