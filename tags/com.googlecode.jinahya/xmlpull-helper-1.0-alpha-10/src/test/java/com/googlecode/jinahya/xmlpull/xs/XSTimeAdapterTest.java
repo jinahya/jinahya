0a9f50a901425be7f2ca19e64925684803f719ff
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-11-22 08:19:26 +0000

    [maven-release-plugin]  copy for tag xmlpull-helper-1.0-alpha-10

---
+++ tags/com.googlecode.jinahya/xmlpull-helper-1.0-alpha-10/src/test/java/com/googlecode/jinahya/xmlpull/xs/XSTimeAdapterTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2011 Jin Kwon <jinahya at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.xmlpull.xs;
+
+
+import java.io.IOException;
+
+import java.util.Date;
+
+import org.testng.annotations.Test;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+
+/**
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+public class XSTimeAdapterTest extends XSTemporalAdapterTest<XSTimeAdapter> {
+
+
+    private static final String[] XS_TIME_STRINGS = new String[]{
+        "21:32:52", "21:32:52+02:00", "19:32:52Z", "19:32:52+00:00",
+        "21:32:52.12679"
+    };
+
+
+    @Test
+    public void testParse() throws XmlPullParserException, IOException {
+
+        testParse(new XSTimeAdapter(), XS_TIME_STRINGS);
+    }
+
+
+    @Test
+    public void testSerialize() throws IOException {
+
+        testSerialize(new XSTimeAdapter(), new Date());
+    }
+
+
+}
+
