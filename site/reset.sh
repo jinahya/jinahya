6478974e880e0b3afa9844b290892691805d4474
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-11-23 13:00:55 +0000

    updated

--- site/reset.sh
+++ site/reset.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-current=`pwd`
-echo $current
-svn up $current
-$current/fix_newlines.sh
-$current/svn_apply_autoprops.py
-svn ci -m reset $current
+#current=`pwd`
+#echo $current
+#svn up $current
+./fix_newlines.sh
+./svn_apply_autoprops.py
+svn ci -m reset
