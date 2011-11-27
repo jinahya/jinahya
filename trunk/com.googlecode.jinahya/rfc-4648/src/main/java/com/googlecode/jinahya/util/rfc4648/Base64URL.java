0d5ed04bc1cf20ce758c0076b3a5068a0b7ded85
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-11-27 07:09:12 +0000

    updated

--- trunk/com.googlecode.jinahya/rfc-4648/src/main/java/com/googlecode/jinahya/util/rfc4648/Base64URL.java
+++ trunk/com.googlecode.jinahya/rfc-4648/src/main/java/com/googlecode/jinahya/util/rfc4648/Base64URL.java
@@ -29,7 +29,7 @@ public class Base64URL extends Base {
 
 
     /**
-     * base64url alphabet.
+     * alphabet for 'base64url'.
      */
     private static final byte[] ALPHABET = new byte[]{
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
