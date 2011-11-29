0a773e112170af96e45c9e196eefa7458573b19b
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-11-29 08:26:17 +0000

    updated

---
+++ trunk/com.googlecode.jinahya/bit-accessible/src/test/java/com/googlecode/jinahya/util/SampleTest.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2011 Jin Kwon <jinahya at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.util;
+
+
+import com.googlecode.jinahya.io.BitOutput;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+
+
+/**
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+public class SampleTest {
+
+
+    @Test
+    public void test() throws IOException {
+
+        final int count = 128;
+        final List<Sample> list = new ArrayList<Sample>(count);
+        for (int i = 0; i < count; i++) {
+            list.add(Sample.newInstance());
+        }
+
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        baos.reset();
+        final BitOutput bo = new BitOutput(baos);
+        for (Sample sample : list) {
+            sample.write(bo);
+        }
+        baos.flush();
+
+        final int bol = baos.toByteArray().length;
+        System.out.println("bo: " + bol);
+
+        baos.reset();
+        final ObjectOutputStream oos = new ObjectOutputStream(baos);
+        for (Sample sample : list) {
+            sample.write(oos);
+        }
+        oos.flush();
+        baos.flush();
+
+        final int ool = baos.toByteArray().length;
+        System.out.println("oo: " + ool);
+
+        System.out.println((float) bol / (float) ool);
+    }
+
+
+}
+
