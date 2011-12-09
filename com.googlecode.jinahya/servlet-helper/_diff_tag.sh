6fc7bb0384430a1ef96a5130b643e26a303a73f1
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-12-09 14:11:42 +0000

    servlet-helper removed; jinahya-ee

--- com.googlecode.jinahya/servlet-helper/_diff_tag.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-groupId=com.googlecode.jinahya
-artifactId=servlet-helper
-svn diff --old=http://jinahya.googlecode.com/svn/tags/$groupId/$artifactId-$1 --new http://jinahya.googlecode.com/svn/trunk/$groupId/$artifactId
--- com.googlecode.jinahya/servlet-helper/_list_tag.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-groupId=com.googlecode.jinahya
-artifactId=servlet-helper
-path=../../../tags/$groupId/
-svn up $path
-ls -l $path | grep $artifactId
