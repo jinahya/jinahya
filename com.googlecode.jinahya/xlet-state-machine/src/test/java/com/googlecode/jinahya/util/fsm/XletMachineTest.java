793af27fc8950c94b4fe4551db0ab3fca9faa954
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2011-12-20 06:05:08 +0000

    trunk has been moved to root

---
+++ com.googlecode.jinahya/xlet-state-machine/src/test/java/com/googlecode/jinahya/util/fsm/XletMachineTest.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2011 onacit.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.util.fsm;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+/**
+ * 
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+public class XletMachineTest {
+
+
+    private static final ClassLoader CLASS_LOADER =
+        XletMachineTest.class.getClassLoader();
+
+
+    private static final ResourceLoader RESOURCE_LOADER =
+        new ClassResourceLoader(CLASS_LOADER);
+
+
+    private static final TaskContext TASK_CONTEXT;
+
+
+    static {
+        try {
+            TASK_CONTEXT = TaskContext.newInstance(
+                "com.googlecode.jinahya.util.fsm", RESOURCE_LOADER,
+                CLASS_LOADER);
+        } catch (FSMException fsme) {
+            throw new InstantiationError(fsme.getMessage());
+        }
+    }
+
+
+    @Test
+    public void testInitialState() {
+
+        final Machine machine = new XletMachine(TASK_CONTEXT);
+
+        Assert.assertSame(machine.getState(), State.UNKNOWN);
+    }
+
+
+    @Test
+    public void testStateChanges() throws FSMException {
+
+        // LOADED -> DESTROYED
+        {
+            final Machine machine = new XletMachine(TASK_CONTEXT);
+            machine.setState(XletState.LOADED);
+            machine.setState(XletState.DESTROYED);
+        }
+
+        // LOADED -> PAUSED -> DESTROYED
+        {
+            final Machine machine = new XletMachine(TASK_CONTEXT);
+            machine.setState(XletState.LOADED);
+            machine.setState(XletState.PAUSED);
+            machine.setState(XletState.DESTROYED);
+        }
+
+        // LOADED -> PAUSED -> ACTIVE -> DESTROYED
+        {
+            final Machine machine = new XletMachine(TASK_CONTEXT);
+            machine.setState(XletState.LOADED);
+            machine.setState(XletState.PAUSED);
+            machine.setState(XletState.ACTIVE);
+            machine.setState(XletState.DESTROYED);
+        }
+
+        // LOADED -> PAUSED -> ACTIVE -> PAUSED
+        {
+            final Machine machine = new XletMachine(TASK_CONTEXT);
+            machine.setState(XletState.LOADED);
+            machine.setState(XletState.PAUSED);
+            machine.setState(XletState.ACTIVE);
+            machine.setState(XletState.PAUSED);
+        }
+    }
+}
