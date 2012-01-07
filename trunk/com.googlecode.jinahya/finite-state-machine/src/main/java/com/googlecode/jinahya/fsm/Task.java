58f87f156a001150092b4207290c7061feb4b04a
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2012-01-07 03:49:59 +0000

    updated

---
+++ trunk/com.googlecode.jinahya/finite-state-machine/src/main/java/com/googlecode/jinahya/fsm/Task.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2011 <a href="mailto:jinahya@gmail.com">Jin Kwon</a>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.fsm;
+
+
+/**
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ */
+public abstract class Task {
+
+
+    /**
+     * Creates a new instance.
+     *
+     * @param id task id
+     */
+    public Task(final String id) {
+        super();
+
+        if (id == null) {
+            throw new NullPointerException("null id");
+        }
+
+        if (id.length() == 0) {
+            throw new IllegalArgumentException("empty id");
+        }
+
+        this.id = id;
+    }
+
+
+    /**
+     * Returns identifier.
+     *
+     * @return id
+     */
+    public final String getId() {
+        return id;
+    }
+
+
+    /**
+     * Checks if given <code>transition</code> matches to any of target
+     * matchers.
+     *
+     * @param transition transition to match.
+     * @return true if matches; false otherwise
+     */
+    public abstract boolean matches(Transition transition);
+
+
+    /**
+     * Prepares for given <code>context</code>.
+     *
+     * @param context transition context
+     * @throws FSMException if an error occurs.
+     */
+    public abstract void prepare(PreparationContext context)
+        throws FSMException;
+
+
+    /**
+     * Performs desired actions for given <code>transition</code>.
+     *
+     * @param context transition context
+     * @throws FSMException if an error occurs.
+     */
+    public abstract void perform(TransitionContext context) throws FSMException;
+
+
+    /** id. */
+    private final String id;
+
+
+}
+
