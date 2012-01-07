68c8f1879cbfea08df3a8f8be5893d2a63e7cca7
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2012-01-07 06:50:35 +0000

    [maven-release-plugin]  copy for tag xlet-state-machine-1.0-alpha-17

---
+++ tags/com.googlecode.jinahya/xlet-state-machine-1.0-alpha-17/tags/xlet-state-machine-1.0-alpha-13/src/main/java/com/googlecode/jinahya/util/fsm/XletTransitionMatcher.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2011 <a href="mailto:jinahya@gmail.com">Jin Kwon</a>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.util.fsm;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+
+/**
+ * Transition matchers.
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ */
+public final class XletTransitionMatcher {
+
+
+    /**
+     * Transition matcher for Xlet loading.
+     */
+    public static final TransitionMatcher LOAD_XLET = new TransitionMatcher() {
+
+
+        @Override
+        public boolean matches(final Transition transition) {
+            return transition.getSource().equals(State.UNKNOWN)
+                   && transition.getTarget().equals(XletState.LOADED);
+        }
+    };
+
+
+    /**
+     * Transition matcher for <code>initXlet</code>.
+     */
+    public static final TransitionMatcher INIT_XLET = new TransitionMatcher() {
+
+
+        @Override
+        public boolean matches(final Transition transition) {
+            return transition.getSource().equals(XletState.LOADED)
+                   && transition.getTarget().equals(XletState.PAUSED);
+        }
+    };
+
+
+    /**
+     * Transition matcher for <code>startXlet</code>.
+     */
+    public static final TransitionMatcher START_XLET = new TransitionMatcher() {
+
+
+        @Override
+        public boolean matches(final Transition transition) {
+            return (transition.getSource().equals(XletState.PAUSED)
+                    && transition.getTarget().equals(XletState.ACTIVE));
+        }
+    };
+
+
+    /**
+     * Transition matcher for <code>pauseXlet</code>.
+     */
+    public static final TransitionMatcher PAUSE_XLET = new TransitionMatcher() {
+
+
+        @Override
+        public boolean matches(final Transition transition) {
+            return (transition.getSource().equals(XletState.ACTIVE)
+                    && transition.getTarget().equals(XletState.PAUSED));
+        }
+    };
+
+
+    /**
+     * Transition matcher for <code>destroyXlet</code>.
+     */
+    public static final TransitionMatcher DESTROY_XLET =
+        new TransitionMatcher() {
+
+
+            @Override
+            public boolean matches(final Transition transition) {
+                return ((transition.getSource().equals(XletState.LOADED)
+                         || transition.getSource().equals(XletState.PAUSED)
+                         || transition.getSource().equals(XletState.ACTIVE))
+                        && transition.getTarget().equals(XletState.DESTROYED));
+            }
+        };
+
+
+    /**
+     * VALUES.
+     */
+    public static final List<TransitionMatcher> VALUES =
+        Arrays.asList(LOAD_XLET, INIT_XLET, START_XLET, PAUSE_XLET,
+                      DESTROY_XLET);
+
+
+    /** PRIVATE. */
+    private XletTransitionMatcher() {
+        super();
+    }
+}
