d99944b0dc6d224df29ecc779e6a7d12f383d680
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2012-01-07 05:41:03 +0000

    [maven-release-plugin]  copy for tag finite-state-machine-1.0-alpha-21

---
+++ finite-state-machine-1.0-alpha-21/src/main/java/com/googlecode/jinahya/fsm/Machine.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2011 <a href="mailto:jinahya@gmail.com">Jin Kwon</a>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.fsm;
+
+
+import java.util.Collections;
+import java.util.List;
+
+
+/**
+ * A Finite State Machine.
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ */
+public abstract class Machine {
+
+
+    /**
+     * Creates a new instance.
+     *
+     * @param tasks tasks
+     */
+    public Machine(final List<Task> tasks) {
+        super();
+
+        if (tasks == null) {
+            throw new NullPointerException("null tasks");
+        }
+
+        if (tasks.isEmpty()) {
+            throw new NullPointerException("empty tasks");
+        }
+
+        this.tasks = Collections.unmodifiableList(tasks);
+    }
+
+
+    /**
+     * Returns current state of this machine.
+     *
+     * @return current state
+     */
+    public final synchronized State getState() {
+        return state;
+    }
+
+
+    /**
+     * Sets state.
+     *
+     * @param state new state
+     * @throws FSMException if an error occurs.
+     */
+    public final synchronized void setState(final State state)
+        throws FSMException {
+
+        if (state == null) {
+            throw new NullPointerException("null state");
+        }
+
+        if (this.state.equals(state)) {
+            throw new FSMException("same state");
+        }
+
+        if (finished) {
+            if (!state.equals(State.INVALID)) {
+                throw new FSMException("already finished");
+            }
+        }
+
+        final Transition transition = new Transition(this.state, state);
+
+        if (!isAllowed(transition)) {
+            throw new FSMException("not allowed: " + transition);
+        }
+
+        if (!started) {
+            if (isStarting(transition)) {
+                started = true;
+            } else {
+                throw new FSMException("not started yet");
+            }
+        }
+
+        for (Task task : tasks) {
+            task.perform(transition);
+        }
+
+        if (isFinishing(transition)) {
+            finished = true;
+        }
+
+        this.state = state;
+
+        if (finished) {
+            setState(State.INVALID);
+        }
+    }
+
+
+    /**
+     * Checks if given <code>transition</code> is a starting condition.
+     * Default implementation always returns <code>true</code>. Override this
+     * method if customizations are needed.
+     *
+     * @param transition transition to check
+     * @return true if given <code>transition</code> is a starting condition;
+     *         false otherwise.
+     */
+    protected abstract boolean isStarting(Transition transition);
+
+
+    /**
+     * Checks if given <code>transition</code> is allowed. Default
+     * implementation always returns <code>true</code>. Override this method if
+     * customizations are needed.
+     *
+     * @param transition transition to check.
+     * @return true if given <code>transition</code> is allowed; false if not
+     *         allowed.
+     */
+    protected abstract boolean isAllowed(Transition transition);
+
+
+    /**
+     * Checks if given <code>transition</code> is an finishing condition.
+     * Default implementation always returns <code>false</code>. Override this
+     * method if customizations are needed.
+     *
+     * @param transition transition to check
+     * @return true if given <code>transition</code> is a finishing condition;
+     *         false otherwise.
+     */
+    protected abstract boolean isFinishing(Transition transition);
+
+
+    /** tasks. */
+    private final transient List<Task> tasks;
+
+
+    /** flag for started. */
+    private volatile boolean started = false;
+
+
+    /** flag for finished. */
+    private volatile boolean finished = false;
+
+
+    /** current state. */
+    private volatile State state = State.UNKNOWN;
+
+
+}
+
---
+++ finite-state-machine-1.0-alpha-21/src/main/java/com/googlecode/jinahya/fsm/ResourceLoader.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2011 <a href="mailto:jinahya@gmail.com">Jin Kwon</a>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.fsm;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+/**
+ * Platform specific resource loader.
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ */
+public interface ResourceLoader {
+
+
+    /**
+     * Loads resource denoted by given <code>resourceName</code>.
+     *
+     * @param name resource name
+     * @return loaded resource stream; never null
+     * @throws IOException if an I/O error occurs
+     * @throws FSMException if an error occurs
+     */
+    InputStream loadResource(String name) throws IOException, FSMException;
+}
