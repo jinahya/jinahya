d32495b1d74cf693a3275f5b875dee907a6966a3
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2012-01-07 03:49:59 +0000

    updated

---
+++ com.googlecode.jinahya/finite-state-machine/src/test/java/com/googlecode/jinahya/fsm/StateImpl.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2011 onacit.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.fsm;
+
+
+import com.googlecode.jinahya.fsm.AbstractState;
+
+
+
+/**
+ *
+ * @author onacit
+ */
+public class StateImpl extends AbstractState {
+
+
+    /** GENERATED. */
+    private static final long serialVersionUID = 6763674645080209972L;
+
+
+    public StateImpl(int code, String name) {
+        super(code, name);
+    }
+}
---
+++ com.googlecode.jinahya/finite-state-machine/src/test/java/com/googlecode/jinahya/fsm/b/B1.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2011 onacit.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.fsm.b;
+
+
+import com.googlecode.jinahya.fsm.AbstractTask;
+import com.googlecode.jinahya.fsm.FSMException;
+import com.googlecode.jinahya.fsm.PreparationContext;
+import com.googlecode.jinahya.fsm.TransitionContext;
+import com.googlecode.jinahya.fsm.TransitionMatcher;
+
+
+/**
+ *
+ * @author onacit
+ */
+public class B1 extends AbstractTask {
+
+
+    protected static final String ID = B1.class.getSimpleName();
+
+
+    public B1() {
+        super(ID, TransitionMatcher.YES);
+    }
+
+
+    @Override
+    public void prepare(final PreparationContext context) throws FSMException {
+        System.out.println("Task(" + getId() + ").prepare(" + context + ")");
+        context.setPerformAfter(B0.ID);
+    }
+
+
+    @Override
+    public void perform(final TransitionContext context) throws FSMException {
+        System.out.println("Task(" + getId() + ").perform(" + context + ")");
+    }
+}
