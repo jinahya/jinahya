6f50f6cce818b2c5d31342bd3e58bb9e8a6a7299
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2012-01-29 17:49:07 +0000

    updated

---
+++ com.googlecode.jinahya/maven-pom-reference/src/main/java/com/googlecode/jinahya/App.java
@@ -0,0 +1,19 @@
+
+
+package com.googlecode.jinahya;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+
+
+}
+
---
+++ com.googlecode.jinahya/maven-pom-reference/src/test/java/com/googlecode/jinahya/AppTest.java
@@ -0,0 +1,23 @@
+
+
+package com.googlecode.jinahya;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+
+    @Test
+    public void testApp() {
+        Assert.assertTrue(true);
+    }
+
+
+}
+
