4d3f20b0d4bb3687f34e334ecbc1eac5476243f7
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2012-02-29 02:18:59 +0000

    updated

--- trunk/com.googlecode.jinahya/id-codec/src/test/java/com/googlecode/jinahya/util/IdCodecTest.java
+++ trunk/com.googlecode.jinahya/id-codec/src/test/java/com/googlecode/jinahya/util/IdCodecTest.java
@@ -36,54 +36,28 @@ public class IdCodecTest {
     private static void test(final long decoded) {
         final String encoded = IdEncoder.encodeId(decoded);
         Assert.assertEquals(IdDecoder.decodeId(encoded), decoded);
-        System.out.println(decoded + " / " + encoded);
+        System.out.println("codec: " + decoded + " / " + encoded);
     }
 
 
-    //@Test(invocationCount = 512)
-    public void test() {
-
-        final long decoded = RANDOM.nextLong() & Long.MAX_VALUE;
-
-        final String encoded = IdEncoder.encodeId(decoded);
-
-        Assert.assertEquals(IdDecoder.decodeId(encoded), decoded);
-    }
-
-
-    //@Test
-    public void test2() {
-
-        for (long decoded = 100000L; decoded < 100100L; decoded++) {
-            final String encoded = IdEncoder.encodeId(decoded);
-            Assert.assertEquals(IdDecoder.decodeId(encoded), decoded);
-            System.out.println(decoded + " / " + encoded);
-        }
-    }
-
-
-    //@Test
-    public void testZero() {
-        test(0L);
-    }
-
-
-    //@Test
-    public void testOne() {
-        test(1L);
+    @Test(invocationCount = 1024)
+    public void testRandom() {
+        test(RANDOM.nextLong());
     }
 
 
     @Test
-    public void testMax() {
-        test(Long.MAX_VALUE);
-    }
+    public void test() {
 
+        for (long decoded = 0L; decoded < 100L; decoded++) {
+            test(decoded);
+        }
 
-    //@Test
-    public void testAny() {
         test(1000000L);
         test(1000001L);
+
+        test(Long.MIN_VALUE);
+        test(Long.MAX_VALUE);
     }
 
 
