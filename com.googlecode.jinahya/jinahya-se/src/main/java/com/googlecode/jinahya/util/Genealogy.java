b938d94f34b7800fd2eef9b41ac190b4b457bd64
onacit@gmail.com <onacit@gmail.com@e3df469a-503a-0410-a62b-eff8d5f2b914>
2012-03-28 15:20:19 +0000

    updated

---
+++ com.googlecode.jinahya/jinahya-se/src/main/java/com/googlecode/jinahya/util/Genealogy.java
@@ -0,0 +1,292 @@
+/*
+ * Copyright 2012 Jin Kwon <jinahya at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.util;
+
+
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+
+/**
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+public final class Genealogy {
+
+
+    /**
+     * Casts or wraps given
+     * <code>value</code> as a
+     * <code>Collection</code>.
+     *
+     * @param <T> type parameter
+     * @param type type
+     * @param value the value
+     *
+     * @return an instance of value collection
+     */
+    @SuppressWarnings({"unchecked"})
+    private static <T> Collection<T> asCollection(final Class<T> type,
+                                                  final Object value) {
+
+        if (value == null) {
+            return Collections.<T>emptyList();
+        }
+
+        if (value instanceof Collection<?>) {
+            return Collections.checkedCollection((Collection<T>) value, type);
+        }
+
+        if (value instanceof Map<?, ?>) {
+            return Collections.checkedCollection(
+                (Collection<T>) ((Map) value).values(), type);
+        }
+
+        return Collections.singleton(type.cast(value)); // ClassCastException
+    }
+
+
+    /**
+     * Returns the value of a property named
+     * <code>name</code> of given
+     * <code>of</code>.
+     *
+     * @param <T> type parameter
+     * @param type type
+     * @param of current object
+     * @param name property name
+     *
+     * @return the property value or null
+     *
+     * @throws IntrospectionException
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     */
+    private static <T> T getPropertyValue(final Class<T> type, final T of,
+                                          final String name)
+        throws IntrospectionException, IllegalAccessException,
+               InvocationTargetException {
+
+        final PropertyDescriptor descriptor =
+            new PropertyDescriptor(name, type);
+        final Method read = descriptor.getReadMethod();
+        if (read == null) {
+            // property can't be read
+            return null;
+        }
+
+        return type.cast(read.invoke(of));
+    }
+
+
+    /**
+     *
+     * @param <T> type parameter
+     * @param type type
+     * @param of the base object
+     * @param name name of the field
+     *
+     * @return the field value
+     *
+     * @throws NoSuchFieldException
+     * @throws IllegalAccessException
+     */
+    private static <T> T getFieldValue(final Class<T> type, final T of,
+                                       final String name)
+        throws NoSuchFieldException, IllegalAccessException {
+
+        final Field field = of.getClass().getDeclaredField(name);
+        if (!field.isAccessible()) {
+            field.setAccessible(true);
+        }
+
+        return type.cast(field.get(of));
+    }
+
+
+    /**
+     *
+     * @param <T> type parameter
+     * @param type type
+     * @param of the base object
+     * @param name the field or property name
+     *
+     * @return the value of given field or property
+     *
+     * @throws NoSuchFieldException
+     * @throws IllegalAccessException
+     * @throws IntrospectionException
+     * @throws InvocationTargetException
+     */
+    private static <T> T getValue(final Class<T> type, final T of,
+                                  final String name)
+        throws NoSuchFieldException, IllegalAccessException,
+               IntrospectionException, InvocationTargetException {
+
+        T value = getFieldValue(type, of, name);
+
+        if (value == null) {
+            value = getPropertyValue(type, of, name);
+        }
+
+        return value;
+    }
+
+
+    /**
+     *
+     * @param <T>
+     * @param type
+     * @param of
+     * @param name
+     *
+     * @return
+     *
+     * @throws NoSuchFieldException
+     * @throws IllegalAccessException
+     * @throws IntrospectionException
+     * @throws InvocationTargetException
+     */
+    private static <T> Collection<T> getValueAsCollection(
+        final Class<T> type, final T of, final String name)
+        throws NoSuchFieldException, IllegalAccessException,
+               IntrospectionException, InvocationTargetException {
+
+        return asCollection(type, getValue(type, of, name));
+    }
+
+
+    /**
+     *
+     * @param <T>
+     * @param type
+     * @param is
+     * @param of
+     * @param names
+     *
+     * @return
+     */
+    public static <T> boolean isAncestor(final Class<T> type, final T is,
+                                         final T of, final String... names) {
+
+        if (type == null) {
+            throw new NullPointerException("null type");
+        }
+
+        if (of == null) {
+            throw new NullPointerException("null of");
+        }
+
+        if (is == null) {
+            return false;
+        }
+
+        if (is == of) {
+            throw new IllegalArgumentException("is == of");
+        }
+
+        if (names == null) {
+            throw new NullPointerException("null names");
+        }
+
+        if (names.length == 0) {
+            throw new IllegalArgumentException("empty names");
+        }
+
+        for (String name : names) {
+            try {
+                for (final T parent : getValueAsCollection(type, of, name)) {
+                    if (is == parent) {
+                        return true;
+                    }
+                    if (isAncestor(type, is, parent, name)) {
+                        return true;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace(System.err);
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+
+    public static <T> boolean isDescendant(final Class<T> type, final T is,
+                                           final T of, final String... names) {
+
+        if (type == null) {
+            throw new NullPointerException("null type");
+        }
+
+        if (is == null) {
+            return false;
+        }
+
+        if (of == null) {
+            throw new NullPointerException("null of");
+        }
+
+        if (is == of) {
+            throw new IllegalArgumentException("is == of");
+        }
+
+        if (names == null) {
+            throw new NullPointerException("null names");
+        }
+
+        if (names.length == 0) {
+            throw new IllegalArgumentException("empty names");
+        }
+
+        for (String name : names) {
+            try {
+                for (T child : getValueAsCollection(type, of, name)) {
+                    if (is == child) {
+                        return true;
+                    }
+                    if (isDescendant(type, is, child, name)) {
+                        return true;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace(System.err);
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+
+    /**
+     * PRIVATE.
+     */
+    private Genealogy() {
+        super();
+    }
+
+
+}
