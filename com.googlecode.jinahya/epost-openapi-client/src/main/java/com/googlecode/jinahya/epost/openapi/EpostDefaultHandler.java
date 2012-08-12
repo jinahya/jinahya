ffd6d5996c7435c0b10b2071f24742119c0ab3ce
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2012-08-12 13:40:01 +0000

    updated

---
+++ com.googlecode.jinahya/epost-openapi-client/src/main/java/com/googlecode/jinahya/epost/openapi/EpostDefaultHandler.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2012 Jin Kwon <jinahya at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.epost.openapi;
+
+
+import java.util.Map;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+
+/**
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+public class EpostDefaultHandler extends DefaultHandler {
+
+
+    public EpostDefaultHandler(final Map<String, String> results) {
+        super();
+
+        if (results == null) {
+            throw new IllegalArgumentException("null results");
+        }
+
+        this.results = results;
+    }
+
+
+    @Override
+    public void characters(final char[] ch, final int start, final int length)
+        throws SAXException {
+
+        System.out.println("characters(" + ch + ", " + start + ", " + length
+                           + ")");
+
+        // by default, do nothing.
+        super.characters(ch, start, length);
+
+        builder.append(ch, start, length);
+    }
+
+
+    @Override
+    public void startElement(final String uri, final String localName,
+                             final String qName, final Attributes attributes)
+        throws SAXException {
+
+        System.out.println("startElement(" + uri + ", " + localName + ", "
+                           + qName + ", " + attributes + ")");
+
+        // by default, do nothing
+        super.startElement(uri, localName, qName, attributes);
+
+        if ("address".equals(qName) || "postcd".equals(qName)) {
+            builder.delete(0, builder.length());
+        }
+    }
+
+
+    @Override
+    public void endElement(final String uri, final String localName,
+                           final String qName)
+        throws SAXException {
+
+        System.out.println("endElement(" + uri + ", " + localName + ", " + qName
+                           + ")");
+
+        // by default, do nothing
+        super.endElement(uri, localName, qName);
+
+        if ("address".equals(qName)) {
+            address = builder.toString();
+        } else if ("postcd".equals(qName)) {
+            postcd = builder.toString();
+        } else if ("item".equals(qName)) {
+            results.put(address, postcd);
+        }
+    }
+
+
+    private final Map<String, String> results;
+
+
+    private final transient StringBuilder builder = new StringBuilder();
+
+
+    private transient String address;
+
+
+    private transient String postcd;
+
+
+}
+
---
+++ com.googlecode.jinahya/epost-openapi-client/src/test/java/com/googlecode/jinahya/epost/openapi/EpostOpenApiClientTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2012 Jin Kwon <jinahya at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.epost.openapi;
+
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import javax.xml.parsers.ParserConfigurationException;
+import org.testng.SkipException;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.xml.sax.SAXException;
+
+
+/**
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+public class EpostOpenApiClientTest {
+
+
+    private static final String[] QUERIES = new String[]{
+        "구의1동"
+    };
+
+
+    @BeforeClass()
+    public void readKey() {
+
+        authKey = System.getProperty("authKey");
+        if (authKey == null) {
+            throw new SkipException("no authKey");
+        }
+    }
+
+
+    @Test
+    public void testCreateContainer()
+        throws IOException, ParserConfigurationException, SAXException {
+
+        final EpostOpenApiClient client = new EpostOpenApiClient(authKey);
+
+        final Map<String, String> results = new LinkedHashMap<String, String>();
+
+        for (String query : QUERIES) {
+            client.query(query, results);
+            for (Entry<String, String> entry : results.entrySet()) {
+                System.out.println("address: " + entry.getKey() + " postcd: "
+                                   + entry.getValue());
+            }
+        }
+    }
+
+
+    private String authKey;
+
+
+}
+
