eff1e7be5fb45954fedc4a27315289c692f986c6
onacit@gmail.com <onacit@gmail.com@e3df469a-503a-0410-a62b-eff8d5f2b914>
2012-08-14 04:23:12 +0000

    updated

---
+++ com.googlecode.jinahya.test/epost-openapi-client-test/src/main/java/com/googlecode/jinahya/epost/openapi/test/Application_.java
@@ -0,0 +1,17 @@
+
+
+package com.googlecode.jinahya.epost.openapi.test;
+
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+/**
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+@ApplicationPath("/rs")
+public class Application_ extends Application {
+}
+
