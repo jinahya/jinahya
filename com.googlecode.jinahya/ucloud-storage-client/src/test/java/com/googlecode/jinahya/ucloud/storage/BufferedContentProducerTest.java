7806b7f846bd110dd5ec95582a3a3b4034823d1f
onacit@gmail.com <onacit@gmail.com@e3df469a-503a-0410-a62b-eff8d5f2b914>
2012-08-20 09:51:18 +0000

    updated

---
+++ com.googlecode.jinahya/ucloud-storage-client/src/test/java/com/googlecode/jinahya/ucloud/storage/BufferedContentProducerTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2012 Jin Kwon <jinahya at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.ucloud.storage;
+
+
+import junit.framework.Assert;
+import org.testng.annotations.Test;
+
+
+/**
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+public class BufferedContentProducerTest {
+
+
+    @Test
+    public void testConstructors() {
+
+        new BufferedContentProducer("application/octet-stream", new byte[0]);
+
+        try {
+            new BufferedContentProducer(null, new byte[0]);
+            Assert.fail("passed: new BufferedContentProducer(null, N/A)");
+        } catch (IllegalArgumentException iae) {
+        }
+
+        try {
+            new BufferedContentProducer("application/octet-stream", null);
+            Assert.fail("passed: new BufferedContentProducer(N/A, null)");
+        } catch (IllegalArgumentException iae) {
+        }
+    }
+}
+
