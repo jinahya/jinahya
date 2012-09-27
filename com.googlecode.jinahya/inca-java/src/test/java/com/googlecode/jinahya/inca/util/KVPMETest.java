9022872690fb9697632d864a2e4686a90aa6532b
onacit@gmail.com <onacit@gmail.com@e3df469a-503a-0410-a62b-eff8d5f2b914>
2012-09-27 01:11:24 +0000

    updated

--- com.googlecode.jinahya/inca-java/src/test/java/com/googlecode/jinahya/inca/util/KVPMETest.java
+++ com.googlecode.jinahya/inca-java/src/test/java/com/googlecode/jinahya/inca/util/KVPMETest.java
@@ -18,7 +18,9 @@
 package com.googlecode.jinahya.inca.util;
 
 
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Map;
 import java.util.Random;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.Assert;
@@ -35,7 +37,56 @@ public class KVPMETest {
     private static final Random RANDOM = new Random();
 
 
-    private void testEncodingDecoding(final Hashtable<String, String> expected) {
+    @Test
+    public static void testEncode() {
+
+        try {
+            KVPME.encode(null);
+            Assert.fail("passed: encode(null)");
+        } catch (IllegalArgumentException iae) {
+            // expected
+        }
+
+        final Hashtable<String, String> decoded =
+            new Hashtable<String, String>();
+
+        try {
+            KVPME.encode(decoded);
+            Assert.fail("passed: encode([EMPTY])");
+        } catch (IllegalArgumentException iae) {
+            // expected
+        }
+
+        decoded.put(RandomStringUtils.random(RANDOM.nextInt(16)),
+                    RandomStringUtils.random(RANDOM.nextInt(16)));
+
+        final String encoded = KVPME.encode(decoded);
+    }
+
+
+    @Test
+    public static void testDecode() {
+
+        try {
+            KVPME.decode(null);
+            Assert.fail("passed: decode(null)");
+        } catch (IllegalArgumentException iae) {
+            // expected
+        }
+
+        try {
+            KVPME.decode("");
+            Assert.fail("passed: decode([EMPTY])");
+        } catch (IllegalArgumentException iae) {
+            // expected
+        }
+
+        final Hashtable decoded = KVPME.decode("=");
+    }
+
+
+    private static void testEncodingDecoding(
+        final Hashtable<String, String> expected) {
 
         final String encoded = KVPME.encode(expected);
 
@@ -46,7 +97,7 @@ public class KVPMETest {
 
 
     @Test(invocationCount = 128)
-    public void testEncodingDecoding() {
+    public static void testEncodingDecoding() {
 
         final Hashtable<String, String> expected =
             new Hashtable<String, String>();
@@ -63,6 +114,48 @@ public class KVPMETest {
         }
 
         testEncodingDecoding(expected);
+    }
+
+
+    @Test(invocationCount = 128)
+    public static void testEncodingAgainstSE() {
+
+        final Hashtable<String, String> expected =
+            new Hashtable<String, String>();
+
+        final int count = RANDOM.nextInt(128) + 1;
+        for (int i = 0; i < count; i++) {
+            final String key = RandomStringUtils.random(RANDOM.nextInt(16));
+            final String value = RandomStringUtils.random(RANDOM.nextInt(16));
+            expected.put(key, value);
+        }
+
+        final String encoded = KVPME.encode(expected);
+
+        final Map<String, String> actual = KVP.decode(encoded);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+
+    @Test(invocationCount = 128)
+    public static void testDecodingAgainstSE() {
+
+
+        final Map<String, String> expected = new HashMap<String, String>();
+
+        final int count = RANDOM.nextInt(128) + 1;
+        for (int i = 0; i < count; i++) {
+            final String key = RandomStringUtils.random(RANDOM.nextInt(16));
+            final String value = RandomStringUtils.random(RANDOM.nextInt(16));
+            expected.put(key, value);
+        }
+
+        final String encoded = KVP.encode(expected);
+
+        final Hashtable<String, String> actual = KVPME.decode(encoded);
+
+        Assert.assertEquals(actual, expected);
     }
 
 
