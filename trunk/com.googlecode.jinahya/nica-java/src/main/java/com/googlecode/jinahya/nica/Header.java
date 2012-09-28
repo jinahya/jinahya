3ceabff9361abb689529705304962708e8cf6f7b
onacit@gmail.com <onacit@gmail.com@e3df469a-503a-0410-a62b-eff8d5f2b914>
2012-09-28 09:04:13 +0000

    updated

---
+++ trunk/com.googlecode.jinahya/nica-java/src/main/java/com/googlecode/jinahya/nica/Header.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2012 Jin Kwon <jinahya at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.nica;
+
+
+/**
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+public enum Header {
+
+
+    /**
+     * Header for initialization vector.
+     */
+    INIT("Inca-Init"),
+    /**
+     * Header for names.
+     */
+    NAME("Inca-Name"),
+    /**
+     * Header for encrypted properties.
+     */
+    CODE("Inca-Code"),
+    /**
+     * Header for HMAC.
+     */
+    AUTH("Inca-Auth");
+
+
+    /**
+     * Creates a new instance.
+     *
+     * @param fieldName http header field-name.
+     */
+    private Header(final String fieldName) {
+//        super();
+
+        if (fieldName == null) {
+            throw new IllegalArgumentException("null fieldName");
+        }
+
+        this.fieldName = fieldName;
+    }
+
+
+    /**
+     * Returns fieldName.
+     *
+     * @return fieldName
+     */
+    public String fieldName() {
+        return fieldName;
+    }
+
+
+    /**
+     * fieldName.
+     */
+    private final String fieldName;
+
+
+}
+
---
+++ trunk/com.googlecode.jinahya/nica-java/src/test/java/com/googlecode/jinahya/nica/util/SHABCTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2012 Jin Kwon <jinahya at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.nica.util;
+
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+
+/**
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+public class SHABCTest extends SHATest<SHABC> {
+
+
+    @Override
+    protected SHABC newInstance() {
+        return new SHABC();
+    }
+
+
+    @Test
+    public void testHashAgainstJCA() {
+
+        final byte[] unhashed = newUnhashed();
+
+        final byte[] actual = newInstance().hash(unhashed);
+
+        final byte[] expected = new SHAJCA().hash(unhashed);
+
+        Assert.assertArrayEquals(actual, expected);
+    }
+
+
+    @Test
+    public void testHashToStringAgainstJCA() {
+
+        final byte[] unhashed = newUnhashed();
+
+        final String actual = newInstance().hashToString(unhashed);
+
+        final String expected = new SHAJCA().hashToString(unhashed);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+
+    @Test
+    public void testHashWithStringAgainstJCA() {
+
+        final String unhashed = newUnhashedString();
+
+        final byte[] actual = newInstance().hash(unhashed);
+
+        final byte[] expected = new SHAJCA().hash(unhashed);
+
+        Assert.assertArrayEquals(actual, expected);
+    }
+
+
+    @Test
+    public void testHashToStringWithStringAgainstJCA() {
+
+        final String unhashed = newUnhashedString();
+
+        final String actual = newInstance().hashToString(unhashed);
+
+        final String expected = new SHAJCA().hashToString(unhashed);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+
+}
+
