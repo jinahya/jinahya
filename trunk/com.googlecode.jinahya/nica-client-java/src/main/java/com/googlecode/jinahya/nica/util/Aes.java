78e025adbdc113a97c0d08dcd4013736556af3b4
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2012-10-26 14:33:23 +0000

    updated

---
+++ trunk/com.googlecode.jinahya/nica-client-java/src/main/java/com/googlecode/jinahya/nica/util/Aes.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2012 Jin Kwon <jinahya at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.nica.util;
+
+
+import java.util.Random;
+
+
+/**
+ * An abstract class for AES encryption/decryption.
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+public abstract class Aes {
+
+
+    /**
+     * AES block size in bits.
+     */
+    public static final int BLOCK_SIZE = 0x80; // 128;
+
+
+    /**
+     * AES block size in bytes.
+     */
+    public static final int BLOCK_SIZE_IN_BYTES = BLOCK_SIZE / 0x08; // 16
+
+
+    /**
+     * Key size in bits.
+     */
+    public static final int KEY_SIZE = 0x100; // 256 0x80; // 128
+
+
+    /**
+     * Key size in bytes.
+     */
+    public static final int KEY_SIZE_IN_BYTES = KEY_SIZE / 0x08; // 32 16
+
+
+    /**
+     * Cipher algorithm.
+     */
+    public static final String ALGORITHM = "AES";
+
+
+    /**
+     * Cipher mode.
+     */
+    public static final String MODE = "CBC";
+
+
+    /**
+     * Cipher padding.
+     */
+    public static final String PADDING = "PKCS5Padding";
+
+
+    /**
+     * Cipher transformation.
+     */
+    public static final String TRANSFORMATION =
+        ALGORITHM + "/" + MODE + "/" + PADDING;
+
+
+    /**
+     * Random.
+     */
+    private static final ThreadLocal RANDOM = new ThreadLocal() {
+
+
+        //@Override
+        protected Object initialValue() {
+            return new Random(System.currentTimeMillis()
+                              * Thread.currentThread().hashCode());
+        }
+
+
+    };
+
+
+    /**
+     * Generates a new iv.
+     *
+     * @return initialization vector
+     */
+    public static byte[] newIv() {
+        final byte[] iv = new byte[BLOCK_SIZE_IN_BYTES];
+        ((Random) RANDOM.get()).nextBytes(iv);
+        return iv;
+    }
+
+
+    /**
+     * Encrypts given
+     * <code>decrypted</code>.
+     *
+     * @param iv initialization vector
+     * @param decrypted the bytes to encrypt
+     *
+     * @return encrypted output
+     */
+    public abstract byte[] encrypt(final byte[] iv, final byte[] decrypted);
+
+
+    /**
+     * Decrypts given
+     * <code>encrypted</code>.
+     *
+     * @param iv initialization vector
+     * @param encrypted the bytes to decrypt
+     *
+     * @return decrypted output
+     */
+    public abstract byte[] decrypt(final byte[] iv, final byte[] encrypted);
+
+
+}
+
