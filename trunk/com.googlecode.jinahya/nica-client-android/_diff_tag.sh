4952aad0c151158949a977eee0885349a32f62bc
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2012-11-02 15:56:52 +0000

    updated

--- trunk/com.googlecode.jinahya/nica-client-android/_diff_tag.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-groupId=com.googlecode.jinahya
-artifactId=nica-android
-svn diff --old=http://jinahya.googlecode.com/svn/tags/$groupId/$artifactId-$1 --new http://jinahya.googlecode.com/svn/trunk/$groupId/$artifactId
--- trunk/com.googlecode.jinahya/nica-client-android/_list_tag.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-groupId=com.googlecode.jinahya
-artifactId=nica-android
-path=../../../tags/$groupId/
-svn up $path
-ls -l $path | grep $artifactId
