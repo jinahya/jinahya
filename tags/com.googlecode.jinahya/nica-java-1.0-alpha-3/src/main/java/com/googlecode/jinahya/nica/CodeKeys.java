2b5b642213444004966d337c96b760c5220639db
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2012-11-03 09:29:09 +0000

    [maven-release-plugin]  copy for tag nica-java-1.0-alpha-3

---
+++ tags/com.googlecode.jinahya/nica-java-1.0-alpha-3/src/main/java/com/googlecode/jinahya/nica/CodeKeys.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright 2012 Jin Kwon <jinahya at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.nica;
+
+
+/**
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+public final class CodeKeys {
+
+
+    /**
+     * Number used once per request.
+     */
+    public static final String REQUEST_NONCE = "REQUEST_NONCE";
+
+
+    /**
+     * Current time in milliseconds since
+     * <code>00:00:00 UTC on 1 January 1970</code>.
+     */
+    public static final String REQUEST_TIMESTAMP = "REQUEST_TIMESTAMP";
+
+
+    /**
+     * User's username, if required, per request.
+     */
+    public static final String USER_USERNAME = "USER_USERNAME";
+
+
+    /**
+     * User's password, if required, per request. The value for this code must
+     * be encoded in SHA-512.
+     */
+    public static final String USER_PASSWORD = "USER_PASSWORD";
+
+
+    /**
+     * 3-letter uppercase country/region code. ISO 3166-1 alpha-3.
+     */
+    public static final String USER_COUNTRY3 = "USER_COUNTRY3";
+
+
+    /**
+     * 2-letter uppercase country/region code. ISO 3166-1 alpha-2.
+     */
+    public static final String USER_COUNTRY2 = "USER_COUNTRY2";
+
+
+    /**
+     * User country/region name in en-US. Not strictly required.
+     */
+    public static final String USER_COUNTRY = "USER_COUNTRY";
+
+
+    /**
+     * 3-letter lowercase language code. ISO 639-2.
+     */
+    public static final String USER_LANGUAGE3 = "USER_LANGUAGE3";
+
+
+    /**
+     * 2-letter lowercase language code. ISO 639-1.
+     */
+    public static final String USER_LANGUAGE2 = "USER_LANGUAGE2";
+
+
+    /**
+     * User language name in en-US. Not strictly required.
+     */
+    public static final String USER_LANGUAGE = "USER_LANGUAGE";
+
+
+    /**
+     * System(OS) version.
+     */
+    public static final String SYSTEM_VERSION = "SYSTEM_VERSION";
+
+
+    /**
+     * System(OS) name.
+     */
+    public static final String SYSTEM_NAME = "SYSTEM_NAME";
+
+
+    /**
+     * System(OS) identifier. Must be, if specified, unique by
+     * {@link #DEVICE_ID}. This code is required if the {@link #DEVICE_ID} is
+     * absent. The value must not be empty nor blank.
+     */
+    public static final String SYSTEM_ID = "SYSTEM_ID";
+
+
+    /**
+     * Device's version.
+     */
+    public static final String DEVICE_VERSION = "DEVICE_VERSION";
+
+
+    /**
+     * Device's name.
+     */
+    public static final String DEVICE_NAME = "DEVICE_NAME";
+
+
+    /**
+     * Device identifier. Must be, if specified, unique by {@link #PLATFORM_ID}.
+     * This code is required if the {@link #SYSTEM_ID} is absent. The value must
+     * not be empty nor blank.
+     */
+    public static final String DEVICE_ID = "DEVICE_ID";
+
+
+    /**
+     * Platform identifier.
+     */
+    public static final String PLATFORM_ID = "PLATFORM_ID";
+
+
+    /**
+     * Creates a new instance.
+     */
+    private CodeKeys() {
+        super();
+    }
+
+
+}
+
---
+++ tags/com.googlecode.jinahya/nica-java-1.0-alpha-3/src/main/java/com/googlecode/jinahya/nica/HeaderFieldNames.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2012 Jin Kwon <jinahya at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.nica;
+
+
+/**
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+public final class HeaderFieldNames {
+
+
+    /**
+     * Header for nica names.
+     */
+    public static final String NAME = "Nica-Name";
+
+
+    /**
+     * Header for initialization vector.
+     */
+    public static final String INIT = "Nica-Init";
+
+
+    /**
+     * Header for encrypted nica codes.
+     */
+    public static final String CODE = "Nica-Code";
+
+
+    /**
+     * Header for nica-code authentication.
+     */
+    public static final String AUTH = "Nica-Auth";
+
+
+    /**
+     * Creates a new instance.
+     */
+    private HeaderFieldNames() {
+        super();
+    }
+
+
+}
+
