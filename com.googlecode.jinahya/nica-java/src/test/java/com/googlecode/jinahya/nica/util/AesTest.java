52cf317a4693eeaece6f5baf65b9dbb872bf4c60
onacit <onacit@e3df469a-503a-0410-a62b-eff8d5f2b914>
2012-11-08 13:38:50 +0000

    updated

--- com.googlecode.jinahya/nica-java/src/test/java/com/googlecode/jinahya/nica/util/AesTest.java
+++ com.googlecode.jinahya/nica-java/src/test/java/com/googlecode/jinahya/nica/util/AesTest.java
@@ -29,7 +29,7 @@ import org.testng.annotations.Test;
 /**
  *
  * @author Jin Kwon <jinahya at gmail.com>
- * @param <A> Aes type parameter
+ * @param <A> {@link Aes} type parameter
  */
 public abstract class AesTest<A extends Aes> {
 
