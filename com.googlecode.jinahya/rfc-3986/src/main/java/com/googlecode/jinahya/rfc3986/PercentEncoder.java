bbb7b05c82c305dc3cb1ed38f2c98cdb3f8b344d
onacit@gmail.com <onacit@gmail.com@e3df469a-503a-0410-a62b-eff8d5f2b914>
2012-11-20 09:26:43 +0000

    updated

--- com.googlecode.jinahya/rfc-3986/src/main/java/com/googlecode/jinahya/rfc3986/PercentEncoder.java
+++ com.googlecode.jinahya/rfc-3986/src/main/java/com/googlecode/jinahya/rfc3986/PercentEncoder.java
@@ -38,6 +38,92 @@ public class PercentEncoder {
 
 
     /**
+     * Converts a 4-bit unsigned integer to a single 7-bit ASCII value. An
+     * <code>IllegalArgumentException</code> will be thrown if given integer is
+     * in wrong range.
+     *
+     * @param integer a 4-bit unsigned integer (0x00 ~ 0x0F)
+     *
+     * @return 7-bit ASCII value; digit (0x30 ~ 0x39), upper alpha (0x41 ~ 0x46)
+     */
+    protected static int encodeHalf(final int decoded) {
+
+        if (decoded >> 4 != 0) {
+            throw new IllegalArgumentException("illegal decoded: " + decoded);
+        }
+
+        return decoded + (decoded < 0x0A ? 0x30 : 0x37);
+        
+        switch (decoded) {
+            case 0x00:
+            case 0x01:
+            case 0x02:
+            case 0x03:
+            case 0x04:
+            case 0x05:
+            case 0x06:
+            case 0x07:
+            case 0x08:
+            case 0x09:
+                return decoded + 0x30; // '0' - '9'
+            case 0x0A:
+            case 0x0B:
+            case 0x0C:
+            case 0x0D:
+            case 0x0E:
+            case 0x0F:
+                return decoded + 0x37; // 'A' - 'F'
+            default:
+                throw new IllegalArgumentException(
+                    "illegal integer: " + decoded);
+        }
+    }
+
+
+    protected static int encodeSingle(final int decoded, final byte[] encoded,
+                                      final int offset) {
+
+        if (decoded >> 8 != 0) {
+            throw new IllegalArgumentException("illegal decoded: " + decoded);
+        }
+
+        if (encoded == null) {
+            throw new IllegalArgumentException("null encoded");
+        }
+
+        if (encoded.length < 3) {
+            // not required
+            throw new IllegalArgumentException(
+                "encoded.length(" + encoded.length + ") < 3");
+        }
+
+        if (offset < 0) {
+            throw new IllegalArgumentException("offset(" + offset + ") < 0");
+        }
+
+        if (offset >= encoded.length - 2) {
+            throw new IllegalArgumentException(
+                "offset(" + offset + ") >= encoded.length(" + encoded.length
+                + ")");
+        }
+
+        if ((decoded >= 0x30 && decoded <= 0x39) // digit
+            || (decoded >= 0x41 && decoded <= 0x5A) // upper case alpha
+            || (decoded >= 0x61 && decoded <= 0x7A) // lower case alpha
+            || decoded == 0x2D || decoded == 0x5F || decoded == 0x2E
+            || decoded == 0x7E) { // -_.~
+            encoded[offset] = (byte) decoded;
+            return 1;
+        } else {
+            encoded[offset] = 0x25; // '%'
+            encoded[offset + 1] = (byte) encodeHalf(decoded >> 4);
+            encoded[offset + 1] = (byte) encodeHalf(decoded & 0x0F);
+            return 3;
+        }
+    }
+
+
+    /**
      * Encodes given
      * <code>input</code>.
      *
@@ -108,7 +194,7 @@ public class PercentEncoder {
         }
 
         final Writer writer = new OutputStreamWriter(output, "US-ASCII");
-        encode(input, writer);
+        encodeSingle(input, writer);
         writer.flush();
     }
 
@@ -142,46 +228,9 @@ public class PercentEncoder {
                 output.write(b);
             } else {
                 output.write(0x25); // '%'
-                output.write(itoa(b >> 4));
-                output.write(itoa(b & 0x0F));
+                output.write(encodeHalf(b >> 4));
+                output.write(encodeHalf(b & 0x0F));
             }
-        }
-    }
-
-
-    /**
-     * Converts a 4-bit unsigned integer to a single 7-bit ASCII value. A
-     * <code>PercentCodecException</code> will be thrown if given
-     * <code>integer</code> is in wrong range.
-     *
-     * @param integer 4-bit unsigned integer (0x00 ~ 0x0F)
-     *
-     * @return 7-bit ASCII value; digit (0x30 ~ 0x39), upper alpha (0x41 ~ 0x46)
-     */
-    private static int itoa(final int i) {
-
-        switch (i) {
-            case 0x00:
-            case 0x01:
-            case 0x02:
-            case 0x03:
-            case 0x04:
-            case 0x05:
-            case 0x06:
-            case 0x07:
-            case 0x08:
-            case 0x09:
-                return i + 0x30; // '0' - '9'
-            case 0x0A:
-            case 0x0B:
-            case 0x0C:
-            case 0x0D:
-            case 0x0E:
-            case 0x0F:
-                return i + 0x37; // 'A' - 'F'
-            default:
-                throw new IllegalArgumentException(
-                    "illegal integer: " + i);
         }
     }
 
@@ -189,8 +238,32 @@ public class PercentEncoder {
     /**
      * Creates a new instance.
      */
-    protected PercentEncoder() {
+    public PercentEncoder() {
         super();
+    }
+
+
+    public byte[] encode(final byte[] decoded) {
+
+        if (decoded == null) {
+            throw new NullPointerException("null decoded");
+        }
+
+        final byte[] encoded = new byte[decoded.length * 3];
+
+        int offset = 0;
+        for (int i = 0; i < decoded.length; i++) {
+            offset += encodeSingle(decoded[i] & 0xFF, encoded, offset);
+        }
+
+        if (offset == encoded.length) {
+            return encoded;
+        }
+
+        final byte[] trimmed = new byte[offset];
+        System.arraycopy(encoded, 0, trimmed, 0, offset);
+
+        return trimmed;
     }
 
 
