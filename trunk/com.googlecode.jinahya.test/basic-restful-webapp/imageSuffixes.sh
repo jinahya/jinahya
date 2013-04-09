52ffced223c1f9c77f8a2c24fd4f7341b6e074ff
onacit@gmail.com <onacit@gmail.com@e3df469a-503a-0410-a62b-eff8d5f2b914>
2013-04-09 13:11:41 +0000

    updated

---
+++ trunk/com.googlecode.jinahya.test/basic-restful-webapp/imageSuffixes.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+curl -s -H "Accept: application/xml" http://localhost:58080/imageSuffixes | xmllint --format - > imageSuffixes.xml
+curl -X PUT -H "Content-Type: application/xml" http://localhost:58080/imageSuffixes --data "@imageSuffixes.xml"
+
+curl -s -H "Accept: application/xml" http://localhost:58080/imageSuffixes/png | xmllint --format - > imageSuffix.xml
+curl -X PUT -H "Content-Type: application/xml" http://localhost:58080/imageSuffixes/png --data "@imageSuffix.xml"
+
+curl -s -H "Accept: application/json" http://localhost:58080/imageSuffixes | python -mjson.tool > imageSuffixes.json
+curl -X PUT -H "Content-Type: application/json" http://localhost:58080/imageSuffixes --data "@imageSuffixes.json"
+
+curl -s -H "Accept: application/json" http://localhost:58080/imageSuffixes/png | python -mjson.tool > imageSuffix.json
+curl -X PUT -H "Content-Type: application/json" http://localhost:58080/imageSuffixes/png --data "@imageSuffix.json"
