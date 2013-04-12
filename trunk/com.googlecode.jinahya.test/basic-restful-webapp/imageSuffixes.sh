64fbc1ed10c1fd1b1a99b0a76535989965203bfa
onacit@gmail.com <onacit@gmail.com@e3df469a-503a-0410-a62b-eff8d5f2b914>
2013-04-12 04:18:26 +0000

    updated

--- trunk/com.googlecode.jinahya.test/basic-restful-webapp/imageSuffixes.sh
+++ trunk/com.googlecode.jinahya.test/basic-restful-webapp/imageSuffixes.sh
@@ -4,29 +4,30 @@ url=$1
 if [ -z "$url" ]; then
     url=http://localhost:58080
 fi
+echo url: $url
 
 echo --------------------------------------------- reading /imageSuffixes as XML
-curl -s -H "Accept: application/xml" $url/imageSuffixes | xmllint --format - > target/imageSuffixes.xml
+curl --connect-timeout 3 -s -H "Accept: application/xml" $url/imageSuffixes | xmllint --format - > target/imageSuffixes.xml
 cat target/imageSuffixes.xml
 echo ------------------------------------------ updating /imageSuffixes with XML
-curl -X PUT -H "Content-Type: application/xml" $url/imageSuffixes --data "@target/imageSuffixes.xml"
+curl --connect-timeout 3 -X PUT -H "Content-Type: application/xml" $url/imageSuffixes --data "@target/imageSuffixes.xml"
 
 echo ----------------------------------------- reading /imageSuffixes/png as XML
-curl -s -H "Accept: application/xml" $url/imageSuffixes/png | xmllint --format - > target/imageSuffix.xml
+curl --connect-timeout 3 -s -H "Accept: application/xml" $url/imageSuffixes/png | xmllint --format - > target/imageSuffix.xml
 cat target/imageSuffix.xml
 echo -------------------------------------- updating /imageSuffixes/png with XML
-curl -X PUT -H "Content-Type: application/xml" $url/imageSuffixes/png --data "@target/imageSuffix.xml"
+curl --connect-timeout 3 -X PUT -H "Content-Type: application/xml" $url/imageSuffixes/png --data "@target/imageSuffix.xml"
 
 
 echo -------------------------------------------- reading /imageSuffixes as JSON
-curl -s -H "Accept: application/json" $url/imageSuffixes | python -mjson.tool > target/imageSuffixes.json
+curl --connect-timeout 3 -s -H "Accept: application/json" $url/imageSuffixes | python -mjson.tool > target/imageSuffixes.json
 cat target/imageSuffixes.json
 echo ----------------------------------------- updating /imageSuffixes with JSON
-curl -X PUT -H "Content-Type: application/json" $url/imageSuffixes --data "@target/imageSuffixes.json"
+curl --connect-timeout 3 -X PUT -H "Content-Type: application/json" $url/imageSuffixes --data "@target/imageSuffixes.json"
 
 echo ---------------------------------------- reading /imageSuffixes/png as JSON
-curl -s -H "Accept: application/json" $url/imageSuffixes/png | python -mjson.tool > target/imageSuffix.json
+curl --connect-timeout 3 -s -H "Accept: application/json" $url/imageSuffixes/png | python -mjson.tool > target/imageSuffix.json
 cat target/imageSuffix.json
 echo ------------------------------------- updating /imageSuffixes/png with JSON
-curl -X PUT -H "Content-Type: application/json" $url/imageSuffixes/png --data "@target/imageSuffix.json"
+curl --connect-timeout 3 -X PUT -H "Content-Type: application/json" $url/imageSuffixes/png --data "@target/imageSuffix.json"
 
