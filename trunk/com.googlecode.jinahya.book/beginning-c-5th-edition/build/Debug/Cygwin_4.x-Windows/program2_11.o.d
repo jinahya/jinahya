2bacea150253938fcb5c35f8f8fcb2a14a517f59
onacit@gmail.com <onacit@gmail.com@e3df469a-503a-0410-a62b-eff8d5f2b914>
2013-05-04 14:49:51 +0000

    updated

---
+++ trunk/com.googlecode.jinahya.book/beginning-c-5th-edition/build/Debug/Cygwin_4.x-Windows/program2_11.o.d
@@ -0,0 +1,3 @@
+build/Debug/Cygwin_4.x-Windows/program2_11.o: program2_11.c program2_11.h
+
+program2_11.h:
---
+++ trunk/com.googlecode.jinahya.book/beginning-c-5th-edition/main.c
@@ -0,0 +1,22 @@
+/*
+ * File:   main.c
+ * Author: onacit
+ *
+ * Created on May 4, 2013, 11:36 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "program2_11.h"
+
+/*
+ *
+ */
+int main(int argc, char** argv) {
+
+  program2_11(argc, argv);
+
+  return (EXIT_SUCCESS);
+}
+
