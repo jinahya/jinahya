27efae38ef2f3dec5f1556dface2524d2138575a
onacit@gmail.com <onacit@gmail.com@e3df469a-503a-0410-a62b-eff8d5f2b914>
2013-05-08 05:42:20 +0000

    updated

--- com.googlecode.jinahya.test/sample-annotations/sample-annotations-tests/src/main/java/com/googlecode/jinahya/test/annotation/FinalTest.java
+++ com.googlecode.jinahya.test/sample-annotations/sample-annotations-tests/src/main/java/com/googlecode/jinahya/test/annotation/FinalTest.java
@@ -7,37 +7,37 @@ package com.googlecode.jinahya.test.annotation;
  *
  * @author Jin Kwon <onacit at gmail.com>
  */
-@Final
-public final class AlmostAllFinal {
+@Final // TYPE
+public final class FinalTest {
 
 
-    @Final
-    public AlmostAllFinal(final String value) {
+    @Final // CONSTRUCTOR
+    public FinalTest(@Final String value /* PARAMETER */) {
         super();
 
         this.value = value;
     }
 
 
-    @Final
-    public AlmostAllFinal() {
+    @Final // METHOD
+    public FinalTest() {
         this(null);
     }
 
 
-    @Final
+    @Final // METHOD
     public String getValue() {
         return value;
     }
 
 
     @Final
-    public void setValue(@Final String value) {
+    public void setValue(@Final String value /* PARAMETER */) {
         this.value = value;
     }
 
 
-    @Final
+    @Final // FIELD
     private String value;
 
 
