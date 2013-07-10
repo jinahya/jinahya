944500a8eff5ed75b283864edc29ef9e63678bce
onacit@gmail.com <onacit@gmail.com@e3df469a-503a-0410-a62b-eff8d5f2b914>
2013-07-10 09:33:22 +0000

    [maven-release-plugin]  copy for tag commons-codec-proxies-1.0-alpha-1

---
+++ tags/com.googlecode.jinahya/commons-codec-proxies-1.0-alpha-1/src/test/java/com/googlecode/jinahya/commons/codec/RareStringEncoderProxyTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2013 Jin Kwon <jinahya at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.commons.codec;
+
+
+import java.lang.reflect.Method;
+import org.apache.commons.codec.StringEncoder;
+import org.apache.commons.codec.EncoderException;
+import org.testng.annotations.Test;
+
+
+/**
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+public class RareStringEncoderProxyTest
+    extends AbstractEncoderProxyTest<RareStringEncoderProxy, RareStringEncoder, StringEncoder> {
+
+
+    public RareStringEncoderProxyTest() {
+        super(RareStringEncoderProxy.class, RareStringEncoder.class,
+              StringEncoder.class);
+    }
+
+
+    @Override
+    protected RareStringEncoder newRareEncoder() {
+
+        return new RareStringEncoder();
+    }
+    @Override
+    protected Object newProxy(final RareStringEncoder rareEncoder) throws Exception {
+        final Method newInstance = StringEncoderProxy.class.getDeclaredMethod(
+            "newInstance", Class.class, Class.class, Object.class);
+        if (!newInstance.isAccessible()) {
+            newInstance.setAccessible(true);
+        }
+        return newInstance.invoke(null, proxyType, rareEncoderType,
+                                  rareEncoder);
+    }
+
+
+    @Override
+    protected void invokeEncode(final StringEncoder commonsEncoder)
+        throws EncoderException {
+
+        commonsEncoder.encode(new String());
+    }
+
+
+}
+
