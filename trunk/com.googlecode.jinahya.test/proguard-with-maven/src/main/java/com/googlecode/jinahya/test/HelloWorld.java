89c0f35ddaec10bff4fa4e7c5b94522332217aae
onacit@gmail.com <onacit@gmail.com@e3df469a-503a-0410-a62b-eff8d5f2b914>
2013-08-02 05:29:14 +0000

    proguard-with-maven added

---
+++ trunk/com.googlecode.jinahya.test/proguard-with-maven/src/main/java/com/googlecode/jinahya/test/HelloWorld.java
@@ -0,0 +1,41 @@
+
+
+package com.googlecode.jinahya.test;
+
+
+public class HelloWorld {
+
+
+    private static final Object private_static_field = null;
+
+
+    protected static final Object protected_static_field = null;
+
+
+    public static final Object public_static_field = null;
+
+
+    private static void private_static_method() {
+    }
+
+
+    protected static void protected_static_method() {
+    }
+
+
+    public static void main(final String[] args) {
+
+        System.out.println("hello, world");
+    }
+
+
+    private Object private_instance_field = null;
+
+
+    protected Object protected_instance_field = null;
+
+
+    public Object public_instance_field = null;
+
+
+}
