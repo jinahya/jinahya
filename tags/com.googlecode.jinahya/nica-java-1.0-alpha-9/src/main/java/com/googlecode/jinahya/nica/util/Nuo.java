fba7b45137bcecf67bd66597b5034d1d998f6d75
onacit@gmail.com <onacit@gmail.com@e3df469a-503a-0410-a62b-eff8d5f2b914>
2013-08-05 05:59:28 +0000

    [maven-release-plugin]  copy for tag nica-java-1.0-alpha-9

---
+++ tags/com.googlecode.jinahya/nica-java-1.0-alpha-9/src/main/java/com/googlecode/jinahya/nica/util/Nuo.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2012 Jin Kwon <jinahya at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.nica.util;
+
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+
+/**
+ * A nonce generator.
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+public class Nuo {
+
+
+    /**
+     * nonce random bits.
+     */
+    private static final int RANDOM_BIT = 20;
+
+
+    /**
+     * nonce random max (exclusive).
+     */
+    private static final int RANDOM_MAX = 1 << RANDOM_BIT;
+
+
+    /**
+     * Generates a value.
+     *
+     * @param timestamp current timestamp.
+     * @param random random.
+     *
+     * @return a new value
+     */
+    protected static long generate(final long timestamp, final Random random) {
+
+        if (random == null) {
+            throw new NullPointerException("random");
+        }
+
+        return (timestamp << RANDOM_BIT) | random.nextInt(RANDOM_MAX);
+    }
+
+
+    /**
+     * Generates a nonce for given {@code timestamp}.
+     *
+     * @param timestamp current timestamp
+     *
+     * @return a new value
+     */
+    public static long generate(final long timestamp) {
+
+        return generate(timestamp, new SecureRandom());
+    }
+
+
+    /**
+     *
+     * @param timestamp
+     *
+     * @return
+     *
+     * @deprecated
+     */
+    public static long gemicro(final long timestamp) {
+
+        return generate(timestamp, new Random());
+    }
+
+
+    /**
+     * Generates a value.
+     *
+     * @param random random
+     *
+     * @return a new value
+     */
+    public static long generate(final Random random) {
+
+        return generate(System.currentTimeMillis(), random);
+    }
+
+
+    /**
+     * Generates a nonce.
+     *
+     * @return a new value
+     */
+    public static long generate() {
+
+        return generate(System.currentTimeMillis());
+    }
+
+
+    /**
+     * Creates a new instance.
+     */
+    protected Nuo() {
+        super();
+    }
+
+
+}
