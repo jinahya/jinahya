9cf8faf445acb8a7cd01326f0e11e85dcff92056
onacit@gmail.com <onacit@gmail.com@e3df469a-503a-0410-a62b-eff8d5f2b914>
2013-08-07 05:26:01 +0000

    updated

---
+++ trunk/com.googlecode.jinahya/nica-java-android-test-impl/src/main/java/com/googlecode/jinahya/nica/client/AndroidNicaBuilderFactoryImpl.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2013 Jin Kwon <onacit at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.nica.client;
+
+
+import android.content.Context;
+import com.googlecode.jinahya.nica.util.Aes;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+
+/**
+ *
+ * @author Jin Kwon <onacit at gmail.com>
+ */
+public class AndroidNicaBuilderFactoryImpl extends AndroidNicaBuilderFactory {
+
+
+    @Override
+    protected ClientCredential loadClientCredential(final Context context)
+        throws NicaClientException {
+
+        return new ClientCredential() {
+
+            @Override
+            public Map getNames() {
+                final Map<String, String> names = new HashMap<String, String>();
+                names.put("key1", "value1");
+                names.put("key2", "value2");
+                return names;
+            }
+
+
+            @Override
+            public byte[] getKey() {
+                final byte[] key = new byte[Aes.KEY_SIZE_IN_BYTES];
+                new Random().nextBytes(key);
+                return key;
+            }
+
+        };
+    }
+
+
+}
