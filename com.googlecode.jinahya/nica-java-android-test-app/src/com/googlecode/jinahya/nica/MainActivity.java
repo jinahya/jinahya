b7c6e7dec6db33bd8f3638497189aa7d477aa671
onacit@gmail.com <onacit@gmail.com@e3df469a-503a-0410-a62b-eff8d5f2b914>
2013-08-07 07:01:35 +0000

    updated

--- com.googlecode.jinahya/nica-java-android-test-app/src/com/googlecode/jinahya/nica/MainActivity.java
+++ com.googlecode.jinahya/nica-java-android-test-app/src/com/googlecode/jinahya/nica/MainActivity.java
@@ -12,6 +12,8 @@ import com.googlecode.jinahya.nica.client.NicaBuilderFactory;
 import com.googlecode.jinahya.nica.client.NicaClientException;
 import java.net.URL;
 import java.net.URLConnection;
+import org.apache.http.HttpRequest;
+import org.apache.http.client.methods.HttpGet;
 
 
 public class MainActivity extends Activity {
@@ -45,13 +47,29 @@ public class MainActivity extends Activity {
             factory.newNicaBuilder().build(connection);
 
             for (Header header : Header.values()) {
-                System.out.println(
-                    header.getName() + ": "
-                    + connection.getRequestProperty(header.getName()));
+                Log.i(TAG, header.getName() + ": "
+                           + connection.getRequestProperty(header.getName()));
             }
         } catch (Exception e) {
             Log.e(TAG, e.getMessage(), e);
         }
+
+        try {
+            final HttpRequest request = new HttpGet("http://www.daum.net");
+            factory.newNicaBuilder().build(request);
+
+            for (Header header : Header.values()) {
+                try {
+                    Log.i(TAG, header.getName() + ": "
+                               + request.getFirstHeader(header.getName())
+                        .getValue());
+                } catch (NullPointerException npe) {
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage(), e);
+        }
+
     }
 
 
