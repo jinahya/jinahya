b00ca670d25a8f67d0b5de40989da6d4a72a2e2b
onacit@gmail.com <onacit@gmail.com@e3df469a-503a-0410-a62b-eff8d5f2b914>
2013-08-21 10:20:43 +0000

    [maven-release-plugin]  copy for tag jinahya-ee-1.0-alpha-3

---
+++ tags/com.googlecode.jinahya/jinahya-ee-1.0-alpha-3/src/main/java/com/googlecode/jinahya/servlet/BufferedServletOutputStream.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2011 Jin Kwon <jinahya at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.servlet;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import javax.servlet.ServletOutputStream;
+
+
+/**
+ * Buffered implementation of ServletOutputStream.
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+public class BufferedServletOutputStream extends ServletOutputStream {
+
+
+    /**
+     * Creates a new instance.
+     */
+    public BufferedServletOutputStream() {
+        super();
+
+        outputStream = new ByteArrayOutputStream();
+    }
+
+
+    @Override
+    public void write(final int b) throws IOException {
+        outputStream.write(b);
+    }
+
+
+    /**
+     * Reset this OutputStream.
+     */
+    public final void reset() {
+        outputStream.reset();
+    }
+
+
+    /**
+     * Returns buffered bytes.
+     *
+     * @return buffered bytes
+     */
+    public final byte[] getBytes() {
+        return outputStream.toByteArray();
+    }
+
+
+    /**
+     * buffer stream.
+     */
+    private final ByteArrayOutputStream outputStream;
+
+
+}
+
---
+++ tags/com.googlecode.jinahya/jinahya-ee-1.0-alpha-3/src/main/java/com/googlecode/jinahya/servlet/http/HttpFilter.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2013 Jin Kwon <onacit at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.servlet.http;
+
+
+import com.googlecode.jinahya.servlet.AbstractFilter;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ *
+ * @author Jin Kwon <onacit at gmail.com>
+ */
+public abstract class HttpFilter extends AbstractFilter {
+
+
+    @Override
+    public void doFilter(final ServletRequest request,
+                         final ServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
+
+        if (!(request instanceof HttpServletRequest)
+            && !(response instanceof HttpServletResponse)) {
+
+            chain.doFilter(request, response);
+
+            return;
+        }
+
+        doFilter((HttpServletRequest) request, (HttpServletResponse) response,
+                 chain);
+    }
+
+
+    protected abstract void doFilter(HttpServletRequest request,
+                                     HttpServletResponse response,
+                                     FilterChain chain)
+        throws IOException, ServletException;
+
+
+}
