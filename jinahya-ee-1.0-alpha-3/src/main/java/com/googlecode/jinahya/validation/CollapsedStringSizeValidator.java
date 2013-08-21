a15119289db2a02d1dcb1b08d8cbddf2fff658de
onacit@gmail.com <onacit@gmail.com@e3df469a-503a-0410-a62b-eff8d5f2b914>
2013-08-21 10:20:43 +0000

    [maven-release-plugin]  copy for tag jinahya-ee-1.0-alpha-3

---
+++ jinahya-ee-1.0-alpha-3/src/main/java/com/googlecode/jinahya/validation/CollapsedStringSizeValidator.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2012 Jin Kwon <jinahya at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.validation;
+
+
+import com.googlecode.jinahya.validation.constraints.CollapsedStringSize;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.xml.bind.annotation.adapters.CollapsedStringAdapter;
+
+
+/**
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+public class CollapsedStringSizeValidator
+    implements ConstraintValidator<CollapsedStringSize, String> {
+
+
+    @Override
+    public void initialize(final CollapsedStringSize constraintAnnotation) {
+        min = constraintAnnotation.min();
+        max = constraintAnnotation.max();
+    }
+
+
+    @Override
+    public boolean isValid(final String value,
+                           final ConstraintValidatorContext context) {
+
+        if (value == null) {
+            return true;
+        }
+
+        final String collapsed = new CollapsedStringAdapter().unmarshal(value);
+
+        return collapsed.length() > min && collapsed.length() < max;
+    }
+
+
+    private int min;
+
+
+    private int max;
+
+
+}
+
---
+++ jinahya-ee-1.0-alpha-3/src/test/java/com/googlecode/jinahya/persistence/Morton.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2013 Jin Kwon <jinahya at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.persistence;
+
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreRemove;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlTransient;
+
+
+/**
+ *
+ * @author Jin Kwon <jinahya at gmail.com>
+ */
+@Entity
+@Table(name = "MORTON")
+public class Morton extends MappedMorton {
+
+
+    /**
+     * generated.
+     */
+    private static final long serialVersionUID = -1421400640928751890L;
+
+
+    /**
+     * logger.
+     */
+    private static final Logger LOGGER =
+        Logger.getLogger(Morton.class.getName());
+
+
+    static {
+        LOGGER.setLevel(Level.INFO);
+    }
+
+
+    /**
+     * default density.
+     */
+    protected static final int DENSITY = MAPPED_DENSITY + 1;
+
+
+    /**
+     * default sodium length.
+     */
+    protected static final int SODIUM_LENGTH = MAPPED_SODIUM_LENGTH << 1;
+
+
+    /**
+     * Creates a new instance.
+     */
+    protected Morton() {
+        super(DENSITY, sodium(SODIUM_LENGTH));
+    }
+
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "@" + hashCode()
+               + "&id=" + id;
+    }
+
+
+    @PrePersist
+    protected void _PrePersist() {
+        LOGGER.log(Level.INFO, "_PrePersist(): {0}", this);
+    }
+
+
+    @PreRemove
+    protected void _PreRemove() {
+        LOGGER.log(Level.INFO, "@PreRemove: {0}", this);
+    }
+
+
+    @Id
+    @GeneratedValue(generator = "MORTON_ID_GENERATOR",
+                    strategy = GenerationType.TABLE)
+    @TableGenerator(initialValue = Pkv.INITIAL_VALUE,
+                    name = "MORTON_ID_GENERATOR",
+                    pkColumnName = Pkv.PK_COLUMN_NAME,
+                    pkColumnValue = "MORTON_ID",
+                    table = Pkv.TABLE,
+                    valueColumnName = Pkv.VALUE_COLUMN_NAME)
+    @NotNull
+    @XmlTransient
+    private Long id;
+
+
+}
+
