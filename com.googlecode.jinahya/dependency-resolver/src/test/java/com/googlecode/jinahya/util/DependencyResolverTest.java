21cb9a09b17c01c3b7a277f9bcd42f1f5e2f2066
onacit@gmail.com <onacit@gmail.com@e3df469a-503a-0410-a62b-eff8d5f2b914>
2013-08-30 10:50:14 +0000

    updated

--- com.googlecode.jinahya/dependency-resolver/src/test/java/com/googlecode/jinahya/util/DependencyResolverTest.java
+++ com.googlecode.jinahya/dependency-resolver/src/test/java/com/googlecode/jinahya/util/DependencyResolverTest.java
@@ -34,130 +34,130 @@ import org.testng.annotations.Test;
 public class DependencyResolverTest {
 
 
-//    @Test
-//    public void testAdd() {
-//
-//        final RawDependencyResolver<String> resolver =
-//            new RawDependencyResolver<String>();
-//
-//        try {
-//            resolver.add(null, "null");
-//            Assert.fail("passed: add(null, E)");
-//        } catch (NullPointerException npe) {
-//            // expected
-//        }
-//
-//        try {
-//            resolver.add("null", (String[]) null);
-//            Assert.fail("passed: add(E, null)");
-//        } catch (NullPointerException npe) {
-//            // expected
-//        }
-//
-//        resolver.add("source", (String) null);
-//        Assert.assertTrue(resolver.contains("source", (String) null));
-//
-//        for (int i = 0; i < 10; i++) {
-//            resolver.add("source", "target" + i);
-//            Assert.assertTrue(resolver.contains("source", "target" + i));
-//        }
-//    }
-//
-//
-//    @Test(expectedExceptions = {IllegalArgumentException.class})
-//    public void testAddForSelfDependency() {
-//
-//        final RawDependencyResolver<String> resolver =
-//            new RawDependencyResolver<String>();
-//
-//        resolver.add("A", "A");
-//    }
-//
-//
-//    @Test(expectedExceptions = {IllegalStateException.class})
-//    public void testAddDependencyForCyclicDependency() {
-//
-//        final RawDependencyResolver<String> resolver =
-//            new RawDependencyResolver<String>();
-//
-//        resolver.add("A", "B");
-//        resolver.add("B", "C");
-//
-//        resolver.add("C", "A");
-//    }
-//
-//
-//    @Test
-//    public void testAddDependencies() {
-//
-//        final RawDependencyResolver<String> resolver =
-//            new RawDependencyResolver<String>();
-//
-//        try {
-//            resolver.add(null, "null");
-//            Assert.fail("passed: null source");
-//        } catch (NullPointerException npe) {
-//            // expected
-//        }
-//
-//        try {
-//            resolver.add("source", (String[]) null);
-//            Assert.fail("passed: null targets");
-//        } catch (NullPointerException npe) {
-//            // expected
-//        }
-//
-//        resolver.add("source", "target1", "target2", null);
-//    }
-//
-//
-//    @Test
-//    public void testContains() {
-//
-//        final RawDependencyResolver<String> resolver =
-//            new RawDependencyResolver<String>();
-//
-//        Assert.assertFalse(resolver.contains("A", "B"));
-//
-//        resolver.add("A", "B");
-//        Assert.assertTrue(resolver.contains("A", "B"));
-//        resolver.remove("A", "B");
-//        Assert.assertFalse(resolver.contains("A", "B"));
-//
-//        resolver.add("A", (String) null);
-//        Assert.assertTrue(resolver.contains("A", null));
-//        resolver.remove("A", (String) null);
-//        Assert.assertFalse(resolver.contains("A", null));
-//    }
-//
-//
-//    @Test
-//    public void testRemove() {
-//
-//        final RawDependencyResolver<String> resolver =
-//            new RawDependencyResolver<String>();
-//
-//        resolver.remove("A", "B");
-//
-//        resolver.add("A", "B");
-//
-//        resolver.remove("A", "B");
-//
-//        resolver.remove("A", "B", "C", "D", null);
-//    }
-//
-//
+    @Test
+    public void testAdd() {
+
+        final DependencyResolver<String> resolver =
+            new DependencyResolver<String>();
+
+        try {
+            resolver.add(null, "null");
+            Assert.fail("passed: add(null, E)");
+        } catch (NullPointerException npe) {
+            // expected
+        }
+
+        try {
+            resolver.add("null", (String[]) null);
+            Assert.fail("passed: add(E, null)");
+        } catch (NullPointerException npe) {
+            // expected
+        }
+
+        resolver.add("source", (String) null);
+        Assert.assertTrue(resolver.contains("source", (String) null));
+
+        for (int i = 0; i < 10; i++) {
+            resolver.add("source", "target" + i);
+            Assert.assertTrue(resolver.contains("source", "target" + i));
+        }
+    }
+
+
+    @Test(expectedExceptions = {IllegalArgumentException.class})
+    public void testAddForSelfDependency() {
+
+        final DependencyResolver<String> resolver =
+            new DependencyResolver<String>();
+
+        resolver.add("A", "A");
+    }
+
+
+    @Test(expectedExceptions = {IllegalStateException.class})
+    public void testAddDependencyForCyclicDependency() {
+
+        final DependencyResolver<String> resolver =
+            new DependencyResolver<String>();
+
+        resolver.add("A", "B");
+        resolver.add("B", "C");
+
+        resolver.add("C", "A");
+    }
+
+
+    @Test
+    public void testAddDependencies() {
+
+        final DependencyResolver<String> resolver =
+            new DependencyResolver<String>();
+
+        try {
+            resolver.add(null, "null");
+            Assert.fail("passed: null source");
+        } catch (NullPointerException npe) {
+            // expected
+        }
+
+        try {
+            resolver.add("source", (String[]) null);
+            Assert.fail("passed: null targets");
+        } catch (NullPointerException npe) {
+            // expected
+        }
+
+        resolver.add("source", "target1", "target2", null);
+    }
+
+
+    @Test
+    public void testContains() {
+
+        final DependencyResolver<String> resolver =
+            new DependencyResolver<String>();
+
+        Assert.assertFalse(resolver.contains("A", "B"));
+
+        resolver.add("A", "B");
+        Assert.assertTrue(resolver.contains("A", "B"));
+        resolver.remove("A", "B");
+        Assert.assertFalse(resolver.contains("A", "B"));
+
+        resolver.add("A", (String) null);
+        Assert.assertTrue(resolver.contains("A", null));
+        resolver.remove("A", (String) null);
+        Assert.assertFalse(resolver.contains("A", null));
+    }
+
+
+    @Test
+    public void testRemove() {
+
+        final DependencyResolver<String> resolver =
+            new DependencyResolver<String>();
+
+        resolver.remove("A", "B");
+
+        resolver.add("A", "B");
+
+        resolver.remove("A", "B");
+
+        resolver.remove("A", "B", "C", "D", null);
+    }
+
+
 //    @Test
 //    public void checkSynchronizedDependencyResolverInheritesAllMethods()
 //        throws NoSuchMethodException {
 //
-//        final RawDependencyResolver<Object> resolver1 =
-//            new RawDependencyResolver<Object>();
+//        final DependencyResolver<Object> resolver1 =
+//            new DependencyResolver<Object>();
 //
 //        final Class<?> class1 = resolver1.getClass();
 //
-//        final RawDependencyResolver<Object> resolver2 =
-//            RawDependencyResolver.sysnchronizedDependencyResolver(resolver1);
+//        final DependencyResolver<Object> resolver2 =
+//            DependencyResolver.sysnchronizedDependencyResolver(resolver1);
 //
 //        final Class<?> class2 = resolver2.getClass();
 //
@@ -181,24 +181,24 @@ public class DependencyResolverTest {
 //            Assert.assertTrue(Modifier.isSynchronized(modifiers2));
 //        }
 //    }
-//
-//
-//    @Test
-//    public void testGetVerticalGroups() {
-//
-//        final RawDependencyResolver<String> resolver =
-//            new RawDependencyResolver<String>();
-//
-//        resolver.add("A", "B");
-//        resolver.add("A", "C");
-//        resolver.add("B", "C");
-//
-//        System.out.println("singleGroup: " + resolver.getSingleGroup());
-//
-//        for (List<String> verticalGroup : resolver.getVerticalGroups()) {
-//            System.out.println("verticalGroup: " + verticalGroup);
-//        }
-//    }
+
+
+    @Test
+    public void testGetVerticalGroups() {
+
+        final DependencyResolver<String> resolver =
+            new DependencyResolver<String>();
+
+        resolver.add("A", "B");
+        resolver.add("A", "C");
+        resolver.add("B", "C");
+
+        System.out.println("singleGroup: " + resolver.getSingleGroup());
+
+        for (List<String> verticalGroup : resolver.getVerticalGroups()) {
+            System.out.println("verticalGroup: " + verticalGroup);
+        }
+    }
 
 
 }
