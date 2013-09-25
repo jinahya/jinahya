0c44117eab837d9b629dde10a7b6da5fc79b4c4e
onacit@gmail.com <onacit@gmail.com@e3df469a-503a-0410-a62b-eff8d5f2b914>
2013-09-25 03:21:52 +0000

    [maven-release-plugin]  copy for tag rfc-4648-1.0.1

---
+++ rfc-4648-1.0.1/src/test/java/com/googlecode/jinahya/rfc4648/Base64UrlEncodingTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2011 Jin Kwon.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.googlecode.jinahya.rfc4648;
+
+
+import org.apache.commons.codec.binary.Base64;
+
+
+/**
+ *
+ * @author <a href="mailto:jinahya@gmail.com">Jin Kwon</a>
+ */
+public class Base64UrlEncodingTest extends DecodingTest<Base64Url, Base64> {
+
+
+    public Base64UrlEncodingTest() {
+        super(Base64Url.class, Base64.class);
+    }
+
+
+    @Override
+    protected Base64 newEncoder() {
+        return new Base64(-1, null, true);
+    }
+
+
+    @Override
+    protected byte[] forBaseDecoding(final byte[] commonsEncoded) {
+        return commonsEncoded;
+    }
+
+
+}
+
